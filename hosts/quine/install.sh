7bf1f5c8e952300cc2a64fd380a814b348aa00f2
Casey <unnamedrambler@gmail.com>
2023-04-23 13:32:21 +0200

    disks.sh fixes

--- hosts/quine/install.sh
+++ hosts/quine/install.sh
@@ -12,19 +12,50 @@ function info {
     echo -e "${BLUE_BG}$1${COLOR_RESET}"
 }
 
+function check_key {
+    if [[ -f "/mnt/persist/etc/ssh/ssh_host_ed25519_key" ]];
+    then
+        echo
+        info "Here we go..."
+    else
+        mkdir -p /mnt/persist/etc/ssh
+        echo
+        echo "Please create /mnt/persist/etc/ssh/ssh_host_ed25519_key"
+        echo "It is used to decrypt secrets."
+        echo
+        read -p "Are you ready? (type 'yes' in capital letters to begin installation): " -r
+        if ! [[ $REPLY == "YES" ]];
+        then
+            err "Aborting"
+            exit 1
+        fi
+        check_key
+    fi
+}
+
+if [[ "$EUID" -gt 0 ]]; then
+    err "Must run as root"
+    exit 1
+fi
+
 if [[ ! -d /mnt/boot ]]; then
     err "No /mnt/boot directory found. Did you run disks.sh first?"
     exit 1
 fi
 
+check_key
+
+chown -R root:root /mnt/persist/etc/ssh/ssh_host_ed25519_key
+chmod 0600 /mnt/persist/etc/ssh/ssh_host_ed25519_key
+
 info "Generating NixOS configuration (/mnt/etc/nixos/*.nix) ..."
 nixos-generate-config --root /mnt
 
 
+cd /home/nixos/nixos-workstation
 rm /mnt/etc/nixos/configuration.nix
-mv /mnt/etc/nixos/hardware-configuration.nix /mnt/etc/nixos/hosts/quine/
+mv /mnt/etc/nixos/hardware-configuration.nix ./hosts/quine/
 
-cd /mnt/etc/nixos/hosts/quine
 
 nixos-install --no-root-passwd \
     --option substituters "https://aseipp-nix-cache.global.ssl.fastly.net" \
