1d1d0af7754af00a79a6995d5993baad7795f9f1
Casey Link <unnamedrambler@gmail.com>
2023-04-24 20:48:53 +0200

    add kde connect firewall rules

--- mixins/kdeconnect.nix
+++ mixins/kdeconnect.nix
@@ -4,6 +4,18 @@
   pkgs,
   ...
 }: {
+  networking.firewall.allowedTCPPortRanges = [
+    {
+      from = 1714;
+      to = 1764;
+    }
+  ];
+  networking.firewall.allowedUDPPortRanges = [
+    {
+      from = 1714;
+      to = 1764;
+    }
+  ];
   home-manager.users.ramblurr = {
     pkgs,
     lib,
