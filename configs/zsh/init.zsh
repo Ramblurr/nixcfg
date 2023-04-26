760cbdb7511b5a6502a1267391ef14e4757a5e73
Casey Link <unnamedrambler@gmail.com>
2023-04-26 16:54:43 +0200

    fixing desktop features

--- configs/zsh/init.zsh
+++ configs/zsh/init.zsh
@@ -1,8 +1,15 @@
 #!/usr/bin/env zsh
 
-source  $ZDOTDIR/autocompletion.zsh .zsh
-source  $ZDOTDIR/environment.zsh
-source  $ZDOTDIR/keybindings.zsh
-source  $ZDOTDIR/prompt.zsh
-source  $ZDOTDIR/privacy.zsh
-source  $ZDOTDIR/gpg-ssh.zsh
+source $ZDOTDIR/autocompletion.zsh .zsh
+source $ZDOTDIR/environment.zsh
+source $ZDOTDIR/keybindings.zsh
+source $ZDOTDIR/privacy.zsh
+source $ZDOTDIR/gpg-ssh.zsh
+
+if command -v starship &>/dev/null; then
+  if [[ $TERM != "dumb" && (-z $INSIDE_EMACS || $INSIDE_EMACS == "vterm") ]]; then
+    eval "$(starship init zsh)"
+  fi
+else
+  source $ZDOTDIR/prompt.zsh
+fi
