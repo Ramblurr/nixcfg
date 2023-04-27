4612a8365a1c9312613bee0137f31140ecf5b63b
Casey Link <unnamedrambler@gmail.com>
2023-04-27 15:11:09 +0200

    add microsocks package

---
+++ packages/microsocks/pkg.nix
@@ -0,0 +1,20 @@
+{
+  stdenv,
+  fetchFromGitHub,
+}:
+stdenv.mkDerivation rec {
+  pname = "microsocks";
+  version = "1.0.3";
+  src = fetchFromGitHub {
+    owner = "rofl0r";
+    repo = "microsocks";
+    rev = "v${version}";
+    fetchSubmodules = true;
+    sha256 = "sha256-l+8+zqeY/Qf5pKvzW9WQBfltEvrD7Vp+hJAXM2o16aI=";
+  };
+
+  installPhase = ''
+    mkdir -p $out/bin/
+    install -m 755 microsocks -t $out/bin/
+  '';
+}
