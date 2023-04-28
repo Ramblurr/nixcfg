bc16a8bb4c602aaaaba069d1613181007e316369
Casey Link <unnamedrambler@gmail.com>
2023-04-28 21:04:59 +0200

    fix polkit auth agent

--- profiles/gui-hyprland.nix
+++ profiles/gui-hyprland.nix
@@ -14,6 +14,19 @@
     hyprland.nvidiaPatches = true;
   };
   security.polkit.enable = true;
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy = ["graphical-session.target"];
+    wants = ["graphical-session.target"];
+    after = ["graphical-session.target"];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
   environment = {
     systemPackages = with pkgs; [
       baobab # Disk usage analyser
