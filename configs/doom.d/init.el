1cc5430b3c8380c64e8e132bb69ad26ebd9f3428
Casey Link <unnamedrambler@gmail.com>
2023-05-08 12:56:41 +0200

    dev tweaks

--- configs/doom.d/init.el
+++ configs/doom.d/init.el
@@ -114,7 +114,7 @@
        ;;agda              ; types of types of types of types...
        ;; beancount         ; mind the GAAP
        ;; ;;(cc +lsp)         ; C > C++ == 1
-       ;; (clojure +lsp)
+       (clojure +lsp)
                                         ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
