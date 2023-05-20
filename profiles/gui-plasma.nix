b2b3311aadbdcc65908eb9c46a40e9a4745bd57e
Casey Link <unnamedrambler@gmail.com>
2023-05-20 22:33:21 +0200

    updates

--- profiles/gui-plasma.nix
+++ profiles/gui-plasma.nix
@@ -8,6 +8,19 @@
   services.xserver.desktopManager.plasma5.runUsingSystemd = false;
 
   home-manager.users.ramblurr = {pkgs, ...} @ hm: {
+    home.packages = [
+      pkgs.libsForQt5.kfind
+      pkgs.krename
+    ];
+    xdg.configFile."baloofilerc".text = ''
+    [Basic Settings]
+    Indexing-Enabled=true
+
+    [General]
+    exclude filters=*~,*.part,*.o,*.la,*.lo,*.loT,*.moc,moc_*.cpp,qrc_*.cpp,ui_*.h,cmake_install.cmake,CMakeCache.txt,CTestTestfile.cmake,libtool,config.status,confdefs.h,autom4te,conftest,confstat,Makefile.am,*.gcode,.ninja_deps,.ninja_log,build.ninja,*.csproj,*.m4,*.rej,*.gmo,*.pc,*.omf,*.aux,*.tmp,*.po,*.vm*,*.nvram,*.rcore,*.swp,*.swap,lzo,litmain.sh,*.orig,.histfile.*,.xsession-errors*,*.map,*.so,*.a,*.db,*.qrc,*.ini,*.init,*.img,*.vdi,*.vbox*,vbox.log,*.qcow2,*.vmdk,*.vhd,*.vhdx,*.sql,*.sql.gz,*.ytdl,*.class,*.pyc,*.pyo,*.elc,*.qmlc,*.jsc,*.fastq,*.fq,*.gb,*.fasta,*.fna,*.gbff,*.faa,po,CVS,.svn,.git,_darcs,.bzr,.hg,CMakeFiles,CMakeTmp,CMakeTmpQmake,.moc,.obj,.pch,.uic,.npm,.yarn,.yarn-cache,__pycache__,node_modules,node_packages,nbproject,core-dumps,lost+found,dist,target,build,.terraform,.terragrunt,.gradle,.m2,logs,.lsp,output,out,*.crdownload,*.part,._*,*.vim*tmp,_lock,_cacache,.cache,.cpcache,.deps,.pioenvs,.piolibdeps,.esphome,stable-diffusion-ui,ovos-buildroot,datomic.data,datomic.data*,redis,redis-dev,.Trash,venv
+    exclude filters version=8
+  '';
+
     home.persistence."/persist/home/ramblurr" = {
       directories = [
         ".config/gtk-3.0" # fuse mounted from /nix/dotfiles/Plasma/.config/gtk-3.0
@@ -29,7 +42,6 @@
       files = [
         ".config/akregatorrc"
         ".config/baloofileinformationrc"
-        ".config/baloofilerc"
         ".config/bluedevilglobalrc"
         ".config/device_automounter_kcmrc"
         ".config/dolphinrc"
@@ -62,7 +74,7 @@
         ".config/kwinrc"
         ".config/kwinrulesrc"
         ".config/kxkbrc"
-        #".config/mimeapps.list"
+        ".config/mimeapps.list"
         ".config/partitionmanagerrc"
         ".config/plasma-localerc"
         ".config/plasma-nm"
