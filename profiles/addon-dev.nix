edfa7153c7ab6f97ef76eeaaad329f57a85e2997
Casey Link <unnamedrambler@gmail.com>
2023-06-14 17:09:13 +0200

    printing

--- profiles/addon-dev.nix
+++ profiles/addon-dev.nix
@@ -30,6 +30,7 @@
       home.file."vendor/jdks/openjdk11".source = pkgs.openjdk11;
       home.file."vendor/jdks/openjdk19".source = pkgs.openjdk19;
       home.packages = with pkgs; [
+        sshpass
         postgresql_15
         mariadb
         git-crypt
