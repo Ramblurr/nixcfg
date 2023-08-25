12e8daa0509c692baad47f72d1adb383ba0b0b4c
Casey Link <unnamedrambler@gmail.com>
2023-08-25 23:47:02 +0200

    refactor to remove config =

--- profiles/gui-wayland.nix
+++ profiles/gui-wayland.nix
@@ -9,28 +9,26 @@
     ./gui.nix
     ../mixins/obs.nix
   ];
-  config = {
-    home-manager.users.ramblurr = {pkgs, ...}: {
-      home.sessionVariables = {
-        XDG_SESSION_TYPE = "wayland";
-        NIXOS_OZONE_WL = "1";
-        MOZ_ENABLE_WAYLAND = "1";
-        _JAVA_AWT_WM_NONREPARENTING = "1";
-      };
-      home.packages = with pkgs; [
-        qt5.qtwayland
-        qt6.qtwayland
-
-        wl-clipboard # wl-{copy,paste}
-        wtype # virtual keystroke insertion
-
-        # imv # image viewer
-        oculante # image viewer (rust)
-        grim # area selection
-        slurp # screen capture
-        wf-recorder # screen record
-        wev # event viewer
-      ];
+  home-manager.users.ramblurr = {pkgs, ...}: {
+    home.sessionVariables = {
+      XDG_SESSION_TYPE = "wayland";
+      NIXOS_OZONE_WL = "1";
+      MOZ_ENABLE_WAYLAND = "1";
+      _JAVA_AWT_WM_NONREPARENTING = "1";
     };
+    home.packages = with pkgs; [
+      qt5.qtwayland
+      qt6.qtwayland
+
+      wl-clipboard # wl-{copy,paste}
+      wtype # virtual keystroke insertion
+
+      # imv # image viewer
+      oculante # image viewer (rust)
+      grim # area selection
+      slurp # screen capture
+      wf-recorder # screen record
+      wev # event viewer
+    ];
   };
 }
