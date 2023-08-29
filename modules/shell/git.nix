5d1aec3eda263e4fb172de1b75773dd0769d2084
Casey Link <unnamedrambler@gmail.com>
2023-08-29 09:26:52 +0200

    add old git aliases from dots

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -33,7 +33,37 @@ in {
           };
         };
         aliases = {
+          s = "status -s";
+          co = "checkout";
+          cob = "checkout -b";
+          la = "!git config -l | grep alias | cut -c 7-";
+          d = "diff";
+          ds = "diff --stat";
+          dc = "diff --cached";
+          a = "add";
+          ap = "add -p";
+          c = "commit --verbose";
+          ca = "commit -a --verbose";
+          cm = "commit -m";
+          cam = "commit -a -m";
+          m = "commit --amend --verbose";
+          # one-line log
+          l = "log --pretty=format:\"%C(yellow)%h\\ %ad%Cred%d\\ %Creset%s%Cblue\\ [%cn]\" --decorate --date=short";
+          rao = "remote add origin";
+          ls = "log --pretty=format:\"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]\" --decorate";
           ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]\" --decorate --numstat";
+          # no colors, for piping
+          lnc = "log --pretty=format:\"%h\\ %s\\ [%cn]\"";
+          lds = "log --pretty=format:\"%C(yellow)%h\\ %ad%Cred%d\\ %Creset%s%Cblue\\ [%cn]\" --decorate --date=short";
+          filelog = "log -u";
+          fl = "log -u";
+          # show modified files in last commit:
+          dl = "!git ll -1";
+          # show a diff last commit:
+          dlc = "diff --cached HEAD^";
+          # find a file path in codebase:
+          f = "!git ls-files | grep -i";
+          logtree = "log --graph --oneline --decorate --all";
         };
 
         difftastic = {
