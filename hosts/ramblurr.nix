829a1dbd83129c133c18ff14a8cfd5dc0d4b0bbc
Casey Link <unnamedrambler@gmail.com>
2023-08-30 14:05:56 +0200

    reuse user config

---
+++ hosts/ramblurr.nix
@@ -0,0 +1,13 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: rec {
+  username = "ramblurr";
+  name = "Casey Link";
+  homeDirectory = "/home/ramblurr";
+  signingKey = "978C4D08058BA26EB97CB51820782DBCACFAACDA";
+  email = "unnamedrambler@gmail.com";
+  passwordSecretKey = "ramblurr-password";
+}
