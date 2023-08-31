1974d24bdb4ff3d2c7ae70e1e9e65717fed6ccdf
Casey Link <unnamedrambler@gmail.com>
2023-08-31 12:39:07 +0200

    mali: update iface names and add more exporters

--- hosts/stable/x86_64-linux/mali/monitoring.nix
+++ hosts/stable/x86_64-linux/mali/monitoring.nix
@@ -4,16 +4,23 @@
   pkgs,
   ...
 }: {
+  environment.systemPackages = with pkgs; [
+    ipmitool
+    lm_sensors
+  ];
   networking.firewall.allowedTCPPorts = [
     config.services.prometheus.exporters.node.port
     config.services.prometheus.exporters.zfs.port
     config.services.prometheus.exporters.smartctl.port
+    config.services.prometheus.exporters.ipmi.port
+    config.services.prometheus.exporters.nut.port
   ];
   services.prometheus = {
     exporters = {
       node = {
         enable = true;
         enabledCollectors = ["systemd"];
+        disabledCollectors = ["textfile"];
         port = 9001;
       };
       zfs = {
@@ -24,8 +31,16 @@
         enable = true;
         port = 9003;
       };
-      # wireguard = { enable = true; };
-      # nginx = { enable = true; };
+      ipmi = {
+        enable = true;
+        port = 9004;
+      };
+      nut = {
+        enable = true;
+        port = 9005;
+        nutUser = "admin";
+        passwordPath = config.sops.secrets.upsAdminPassword.path;
+      };
     };
   };
 }
