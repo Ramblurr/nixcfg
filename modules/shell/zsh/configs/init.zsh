1edd420932053be63237f5931853146149f84392
Casey Link <unnamedrambler@gmail.com>
2023-09-01 11:26:35 +0200

    quine tweaks

--- modules/shell/zsh/configs/init.zsh
+++ modules/shell/zsh/configs/init.zsh
@@ -4,7 +4,7 @@ source $ZDOTDIR/autocompletion.zsh .zsh
 source $ZDOTDIR/environment.zsh
 source $ZDOTDIR/keybindings.zsh
 source $ZDOTDIR/privacy.zsh
-source $ZDOTDIR/gpg-ssh.zsh
+#source $ZDOTDIR/gpg-ssh.zsh
 
 if command -v starship &>/dev/null; then
   if [[ $TERM != "dumb" && (-z $INSIDE_EMACS || $INSIDE_EMACS == "vterm") ]]; then
