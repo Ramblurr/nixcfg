d52f8e82ad6e4d4fe826fb7a43971432cd5718fb
Casey Link <unnamedrambler@gmail.com>
2023-09-01 16:38:54 +0200

    mali: email alerts

---
+++ hosts/stable/x86_64-linux/mali/email.nix
@@ -0,0 +1,79 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  emailTo = "casey@***REMOVED***";
+  emailFrom = "notifications@***REMOVED***";
+
+  hostName = "mali";
+  sendEmailEvent = {event}: ''
+    printf "Subject: ${hostName} ${event} ''$(${pkgs.coreutils}/bin/date --iso-8601=seconds)\n\nzpool status:\n\n''$(${pkgs.zfs}/bin/zpool status)" | ${pkgs.msmtp}/bin/msmtp -a default ${emailTo}
+  '';
+in {
+  programs.msmtp = {
+    enable = true;
+    setSendmail = true;
+    defaults = {
+      aliases = builtins.toFile "aliases" ''
+        default: ${emailTo}
+      '';
+    };
+    accounts = {
+      default = {
+        host = "10.5.0.3";
+        port = 25;
+        from = emailFrom;
+        auth = "off";
+      };
+    };
+  };
+  services.zfs.zed = {
+    enableMail = false;
+    settings = {
+      ZED_NOTIFY_VERBOSE = true;
+      ZED_DEBUG_LOG = "/tmp/zed.debug.log";
+
+      ZED_EMAIL_ADDR = [emailTo];
+      ZED_EMAIL_PROG = "${pkgs.msmtp}/bin/msmtp";
+      ZED_EMAIL_OPTS = "-a 'FROM:${emailFrom}' -s '@SUBJECT@' @ADDRESS@";
+
+      ZED_NOTIFY_INTERVAL_SECS = 3600;
+      ZED_USE_ENCLOSURE_LEDS = true;
+      ZED_SCRUB_AFTER_RESILVER = true;
+    };
+  };
+  systemd.services."boot-mail-alert" = {
+    wantedBy = ["multi-user.target"];
+    after = ["network.target" "network-online.target"];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    script = sendEmailEvent {event = "just booted";};
+  };
+  systemd.services."shutdown-mail-alert" = {
+    wantedBy = ["multi-user.target"];
+    after = ["network.target" "network-online.target"];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    script = "true";
+    preStop = sendEmailEvent {event = "is shutting down";};
+  };
+  systemd.services."weekly-mail-alert" = {
+    serviceConfig.Type = "oneshot";
+    script = sendEmailEvent {event = "is still alive";};
+  };
+  systemd.timers."weekly-mail-alert" = {
+    wantedBy = ["timers.target"];
+    partOf = ["weekly-mail-alert.service"];
+    timerConfig.OnCalendar = "weekly";
+  };
+
+  services.smartd.notifications.mail.enable = true;
+  services.smartd.notifications.mail.sender = emailFrom;
+  services.smartd.notifications.mail.recipient = emailTo;
+}
