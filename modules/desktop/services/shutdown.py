a16d484eb677d3d26b0bff6df42b18e8b500b8e5
Casey Link <unnamedrambler@gmail.com>
2023-09-03 14:06:14 +0200

    ha-shutdown: update shutdown script

--- modules/desktop/services/shutdown.py
+++ modules/desktop/services/shutdown.py
@@ -31,59 +31,65 @@ if SHUTDOWN_BEARER_TOKEN is None:
 
 class WebhookHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-        if self.path == "/shutdown":
-            auth_header = self.headers.get("Authorization", "")
-            token = auth_header.split(" ")[-1]
+        try:
+            if self.path == "/shutdown":
+                auth_header = self.headers.get("Authorization", "")
+                token = auth_header.split(" ")[-1]
 
-            if token != SHUTDOWN_BEARER_TOKEN:
-                self.send_response(401)
+                if token != SHUTDOWN_BEARER_TOKEN:
+                    self.send_response(401)
+                    self.end_headers()
+                    self.wfile.write(json.dumps({"error": "Unauthorized"}).encode())
+                    return
+
+                result = subprocess.run(
+                    [
+                        "dunstify",
+                        "--urgency",
+                        "critical",
+                        "--timeout",
+                        TIMEOUT,
+                        "--appname",
+                        "update",
+                        "--action",
+                        "Y,yes",
+                        "Shutting Down!",
+                        "Click To Stop Shutdown",
+                    ],
+                    capture_output=True,
+                    text=True,
+                )
+
+                output = result.stdout.strip()
+                self.send_response(200)
                 self.end_headers()
-                self.wfile.write(json.dumps({"error": "Unauthorized"}).encode())
+
+                if output == "Y":
+                    self.wfile.write(
+                        json.dumps({"message": "Shutdown aborted"}).encode()
+                    )
+                    print("ABORTING SHUTDOWN")
+                    return
+
+                if output == "1":
+                    if DRY_RUN:
+                        print("Would have powered off")
+                    else:
+                        print("POWERING OFF")
+                        subprocess.run(["systemctl", "poweroff"])
+                    self.wfile.write(json.dumps({"message": "Shutting down"}).encode())
+                    return
+
+                print("Unknown output from dunstify: " + output)
+                self.wfile.write(
+                    json.dumps({"message": "Unknown output from dunstify"}).encode()
+                )
                 return
-
-            result = subprocess.run(
-                [
-                    "dunstify",
-                    "--urgency",
-                    "critical",
-                    "--timeout",
-                    TIMEOUT,
-                    "--appname",
-                    "update",
-                    "--action",
-                    "Y,yes",
-                    "Shutting Down!",
-                    "Click To Stop Shutdown",
-                ],
-                capture_output=True,
-                text=True,
-            )
-
-            output = result.stdout.strip()
-            self.send_response(200)
-            self.end_headers()
-
-            if output == "Y":
-                self.wfile.write(json.dumps({"message": "Shutdown aborted"}).encode())
-                print("ABORTING SHUTDOWN")
-                return
-
-            if output == "1":
-                if DRY_RUN:
-                    print("Would have powered off")
-                else:
-                    print("POWERING OFF")
-                    subprocess.run(["systemctl", "poweroff"])
-                self.wfile.write(json.dumps({"message": "Shutting down"}).encode())
-                return
-
-            print("Unknown output from dunstify: " + output)
-            self.wfile.write(
-                json.dumps({"message": "Unknown output from dunstify"}).encode()
-            )
-            return
+        except Exception as e:
+            print("ERROR: " + str(e))
 
 
+print("Starting shutdown server on port " + str(PORT))
 server_address = ("0.0.0.0", PORT)
 httpd = HTTPServer(server_address, WebhookHandler)
 httpd.serve_forever()
