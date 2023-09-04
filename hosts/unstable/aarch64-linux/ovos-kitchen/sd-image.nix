fc9b97ee858c6f28c762c333ccf017f28856420d
Casey Link <unnamedrambler@gmail.com>
2023-09-04 13:57:24 +0200

    Add sd-image build for ovos-kitchen

---
+++ hosts/unstable/aarch64-linux/ovos-kitchen/sd-image.nix
@@ -0,0 +1,20 @@
+{
+  self,
+  nixos-raspberrypi,
+  ...
+}: {
+  sd-image =
+    (self.nixosConfigurations.ovos-kitchen.extendModules {
+      modules = [
+        nixos-raspberrypi.nixosModules.sd-image-rpi4
+        {
+          system.nixos.variant_id = "ovos";
+          sdImage.imageBaseName = "ovos-kitchen-rpi4-nix-sd-image";
+        }
+      ];
+    })
+    .config
+    .system
+    .build
+    .sdImage;
+}
