975323aede142721aa09d455220f907a0d037aec
Casey Link <unnamedrambler@gmail.com>
2023-09-06 14:27:19 +0200

    ovos-kitchen-sat -> ovos-bedroom

---
+++ hosts/unstable/aarch64-linux/pipewire.nix
@@ -0,0 +1,100 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  security.rtkit.enable = false;
+  hardware.pulseaudio.enable = pkgs.lib.mkForce false;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = false; # ?
+    pulse.enable = false;
+    jack.enable = false;
+    wireplumber.enable = true;
+    audio.enable = true;
+    package = pkgs.pipewire.override {libcameraSupport = false;};
+  };
+
+  systemd.user.services = {
+    pipewire.wantedBy = ["default.target"];
+    wireplumber.wantedBy = ["default.target"];
+  };
+  environment.etc."pipewire/pipewire.conf.d/100-user.conf" = {
+    text =
+      builtins.toJSON
+      {
+        "context.modules" = [
+          {
+            name = "libpipewire-module-rt";
+            args = {
+              "nice.level" = 20;
+              "rt.prio" = 88;
+              "rtportal.enabled" = false;
+              "rtkit.enabled" = false;
+              "rlimits.enabled" = true;
+            };
+            flags = ["ifexists" "nofail"];
+          }
+          {
+            name = "libpipewire-module-loopback";
+            args = {
+              "node.name" = "music-to-speakers-bridge";
+              "node.description" = "music-to-speakers-bridge";
+              "target.delay.sec" = 0;
+              "capture.props" = {
+                "node.target" = "alsa_input.platform-soc_sound.stereo-fallback";
+              };
+              "playback.props" = {
+                "monitor.channel-volumes" = true;
+                "media.role" = "Multimedia";
+                #"device.intended-roles" = "Multimedia";
+              };
+            };
+          }
+        ];
+      };
+  };
+  environment.etc."wireplumber/policy.lua.d/50-endpoints-config.lua".text = ''
+    default_policy.policy.roles = {
+      ["Capture"] = {
+        ["alias"] = {  "Capture" },
+        ["priority"] = 25,
+        ["action.default"] = "cork",
+        ["action.capture"] = "mix",
+        ["media.class"] = "Audio/Source",
+      },
+      ["Multimedia"] = {
+        ["alias"] = { "Movie", "Music", "Game" },
+        ["priority"] = 25,
+        ["action.default"] = "mix",
+      },
+      ["Speech"] = {
+        ["priority"] = 60,
+        ["action.default"] = "duck",
+        ["action.Speech"] = "mix",
+      }
+    }
+
+    default_policy.endpoints = {
+      ["endpoint.capture"] = {
+        ["media.class"] = "Audio/Source",
+        ["role"] = "Capture",
+      },
+      ["endpoint.multimedia"] = {
+        ["media.class"] = "Audio/Sink",
+        ["role"] = "Multimedia",
+      },
+      ["endpoint.speech"] = {
+        ["media.class"] = "Audio/Sink",
+        ["role"] = "Speech",
+      },
+    }
+  '';
+}
+# pw-link  "alsa_input.platform-soc_sound.stereo-fallback:capture_FL" "control.endpoint.multimedia:playback_FL"
+# pw-link "alsa_input.platform-soc_sound.stereo-fallback:capture_FR" "control.endpoint.multimedia:playback_FR"
+# pw-link "roc-recv-source:receive_FR" "control.endpoint.speech_high:playback_FR"
+# pw-link "roc-recv-source:receive_FL" "control.endpoint.speech_high:playback_FL"
+
