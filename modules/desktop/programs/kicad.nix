fa47b6fc5f6154d9771ce51107490e11defce535
Casey Link <unnamedrambler@gmail.com>
2023-09-22 15:16:55 +0200

    renovate: update groups url

---
+++ modules/desktop/programs/kicad.nix
@@ -0,0 +1,37 @@
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  edge,
+  inputs,
+  ...
+}:
+with lib;
+with lib.my; let
+  cfg = config.modules.desktop.programs.kicad;
+  username = config.modules.users.primaryUser.username;
+  homeDirectory = config.modules.users.primaryUser.homeDirectory;
+  withImpermanence = config.modules.impermanence.enable;
+in {
+  options.modules.desktop.programs.kicad = {
+    enable = mkBoolOpt false;
+  };
+  config = mkIf cfg.enable {
+    home-manager.users."${username}" = {
+      pkgs,
+      config,
+      ...
+    } @ hm: {
+      home.packages = [
+        kicad
+      ];
+      home.persistence."/persist${homeDirectory}" = mkIf withImpermanence {
+        directories = [
+          ".config/kicad"
+          ".config/kicad5"
+        ];
+      };
+    };
+  };
+}
