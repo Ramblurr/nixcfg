afa2de669aaf1c3d9e92cb97daf36ba2494d6137
Casey Link <unnamedrambler@gmail.com>
2023-09-30 14:21:18 +0200

    persist chromium

--- modules/desktop/browser/chromium.nix
+++ modules/desktop/browser/chromium.nix
@@ -10,6 +10,7 @@ with lib;
 with lib.my; let
   cfg = config.modules.desktop.browsers.chromium;
   username = config.modules.users.primaryUser.username;
+  homeDirectory = config.modules.users.primaryUser.homeDirectory;
   withImpermanence = config.modules.impermanence.enable;
 in {
   options.modules.desktop.browsers.chromium = {
@@ -21,5 +22,14 @@ in {
     nixpkgs.config.chromium.commandLineArgs =
       mkIf config.modules.desktop.wayland.enable
       "--enable-features=UseOzonePlatform --ozone-platform=wayland";
+
+    home-manager.users."${username}" = {pkgs, ...} @ hm: {
+      home.persistence."/persist${homeDirectory}" = {
+        directories = [
+          ".config/chromium"
+          ".cache/chromium"
+        ];
+      };
+    };
   };
 }
