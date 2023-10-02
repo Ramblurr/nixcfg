0f29660460d4d1e10e448aa723edfdeb918869ca
Casey Link <unnamedrambler@gmail.com>
2023-10-02 10:17:14 +0200

    update clojure doom config

--- modules/editors/emacs/configs/doom.d/+bindings.el
+++ modules/editors/emacs/configs/doom.d/+bindings.el
@@ -43,37 +43,6 @@
   "c"       #'+workspace/close-window-or-workspace
   "C-C"     #'ace-delete-window)
 
- (:after cider-mode
-  (:map cider-mode-map
-   :leader
-   :desc "Lookup documentation at point" :n "d" #'cider-doc
-   :desc "Jump to definition at point" :n "l" #'cider-find-var
-   :localleader
-   ;; :n "b" #'cider-eval-buffer
-   ;; :n "B" #'cider-switch-to-repl-buffer
-   ;; :n "n" #'cider-repl-set-ns
-   ;; :n "j" #'cider-find-var
-   ;; :n "s" #'cider-browse-spec
-   ;; :n "S" #'helm-cider-spec-ns
-   ;; :n "l" #'cljr-move-to-let
-   ;; :n "L" #'cljr-introduce-let
-   ;; (:desc "docs" :prefix "d"
-   ;;   :desc "Browse Namespace" :n "n" #'cider-browse-ns
-   ;;   :desc "Browse Spec" :n "s" #'cider-browse-spec
-   ;;   :desc "Load ClojureDoc" :n "d" #'cider-clojuredocs)
-   ;; :n "h" #'cider-doc
-   ;; :n "c" #'cider-repl-clear-buffer
-   ;; :n "i" #'cider-inspect-last-result
-   ;; :n "p" #'cider-eval-sexp-at-point
-   ;; :n "f" #'cider-eval-defun-at-point
-   ;; :n "t" #'cider-test-run-ns-tests
-   ;; :n "T" #'cider-test-run-test
-   )
-  ;; (:after cider-browse-ns-mode
-  ;;   (:map cider-browse-ns-mode-map
-  ;;     :n "RET"       #'cider-browse-ns-operate-at-point))
-  )
-
 
  ;; Highjacks space/backspace to:
  ;;   a) balance spaces inside brackets/parentheses ( | ) -> (|)
@@ -92,23 +61,6 @@
  ;; :n "C-u"     #'lispyville-delete-back-to-indentation
  )
 
-(map!
-  :after clojure-mode
-  :n "S-<f8>" 'my/clojure-dev-reset
-  :n "C-S-c" 'portal.api/clear
-  ;; :n "C-S-c" 'my/clojure-reveal-clear-output
-
-  )
-
-(map!
-  :leader
-  (:prefix-map ("k" . "structural editing")
-               "dx" #'kill-sexp
-               "dX" #'backward-kill-sexp
-               "c" #'lispy-clone
-               "p" #'cljr-raise
-               "D" #'cljr-destructure-keys
-               ))
 
 (map!
   :leader
@@ -122,20 +74,7 @@
     )
   )
 
-(map!
-  :after clojure-mode
-  :map (clojure-mode-map clojurescript-mode-map clojurec-mode-map)
-  (:localleader
-    (:prefix-map ("e" . "eval")
-      "D" nil
-      "D" 'my/eval-defun-and-reload-browser
-      "c" 'my/eval-rcf
-      ;; "e" 'cider-tap-last-sexp
-      ;; "E" 'cider-eval-last-sexp
-      )
-    (:prefix-map ("n" . "namespace")
-      "s" 'clojure-sort-ns
-      )))
+
 
 (map! :map doom-leader-map "o g" #'elpher)
 
