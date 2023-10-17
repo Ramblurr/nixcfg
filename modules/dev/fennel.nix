1dbe8cec222de7c3d18c4a98c117a153d0430d41
Casey Link <unnamedrambler@gmail.com>
2023-10-17 10:34:31 +0200

    tweak

---
+++ modules/dev/fennel.nix
@@ -0,0 +1,46 @@
+{
+  config,
+  inputs,
+  options,
+  lib,
+  pkgs,
+  my,
+  ...
+}:
+with lib;
+with lib.my; let
+  devCfg = config.modules.dev;
+  cfg = devCfg.fennel;
+  username = config.modules.users.primaryUser.username;
+  homeDirectory = config.modules.users.primaryUser.homeDirectory;
+  withImpermanence = config.modules.impermanence.enable;
+  nur = import inputs.nur {
+    nurpkgs = pkgs;
+    inherit pkgs;
+  };
+in {
+  options.modules.dev.fennel = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      lua
+      luajit
+      lua-language-server
+      fennel
+      fnlfmt
+      nur.repos.bandithedoge.fennel-language-server
+      love
+    ];
+    home-manager.users."${username}" = {
+      #home.packages = with pkgs; [
+      #];
+      home.persistence."/persist${homeDirectory}" = mkIf withImpermanence {
+        directories = [
+          ".config/fennel"
+        ];
+      };
+    };
+  };
+}
