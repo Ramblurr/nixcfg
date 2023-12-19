aca74b2719011a82b22d12c27bf7251487585b94
Casey Link <unnamedrambler@gmail.com>
2023-12-19 09:53:27 +0100

    flatpak tweaks

--- modules/services/flatpak.nix
+++ modules/services/flatpak.nix
@@ -23,6 +23,47 @@ in {
         "/var/lib/flatpak"
       ];
     };
+    # Workaround for https://github.com/NixOS/nixpkgs/issues/119433#issuecomment-1694123978
+    system.fsPackages = [pkgs.bindfs];
+    fileSystems = let
+      mkRoSymBind = path: {
+        device = path;
+        fsType = "fuse.bindfs";
+        options = ["ro" "resolve-symlinks" "x-gvfs-hide"];
+      };
+      aggregatedIcons = pkgs.buildEnv {
+        name = "system-icons";
+        paths = with pkgs; [
+          libsForQt5.breeze-qt5 # for plasma
+          gnome.gnome-themes-extra
+        ];
+        pathsToLink = ["/share/icons"];
+      };
+      aggregatedFonts = pkgs.buildEnv {
+        name = "system-fonts";
+        paths = config.fonts.packages;
+        pathsToLink = ["/share/fonts"];
+      };
+    in {
+      "/usr/share/icons" = mkRoSymBind "${aggregatedIcons}/share/icons";
+      "/usr/local/share/fonts" = mkRoSymBind "${aggregatedFonts}/share/fonts";
+    };
+    systemd.user.services.flatpak-auto-update = {
+      enable = true;
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.flatpak}/bin/flatpak --user update --noninteractive --assumeye";
+      };
+    };
+
+    systemd.user.timers.flatpak-auto-update = {
+      enable = true;
+      description = "Enable automatic flatpak updates";
+      timerConfig = {
+        OnCalendar = "daily";
+        Persistent = "true";
+      };
+    };
     home-manager.users."${username}" = {
       home.persistence."/persist${homeDirectory}" = mkIf withImpermanence {
         directories = [
