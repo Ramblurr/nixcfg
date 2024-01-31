792134061ebdb53540f560cd224d1a61b7ea3931
Casey Link <unnamedrambler@gmail.com>
2024-01-31 15:11:58 +0100

    muse-sounds-manager package

--- packages/default.nix
+++ packages/default.nix
@@ -1,34 +1,19 @@
-{pkgs}:
-pkgs.lib.makeScope pkgs.newScope (self: let
+{pkgs-stable}:
+pkgs-stable.lib.makeScope pkgs-stable.newScope (self: let
+  pkgs = pkgs-stable;
   callPackage = self.callPackage;
 in {
   microsocks = callPackage ./microsocks {};
   hacompanion = callPackage ./hacompanion {};
   bootleg = callPackage ./bootleg {};
-  filetote = callPackage ./filetote {};
-  beets-audible = callPackage ./beets-audible {};
-  beets-filetote = callPackage ./beets-filetote {inherit (pkgs.my) beets-audible;};
+  beets-filetote = callPackage ./beets-filetote {
+    inherit pkgs;
+    poetry-core = pkgs.python311Packages.poetry-core;
+    beets = pkgs.beetsPackages.beets-minimal;
+  };
+  beets-dynamicrange = pkgs.callPackage ./beets-dynamicrange {
+    beets = pkgs.beetsPackages.beets-minimal;
+  };
   muse-sounds-deb = callPackage ./muse-sounds-deb {inherit pkgs;};
-  #muse-sounds-manager = callPackage ./muse-sounds-manager {};
-  muse-sounds-manager = (
-    pkgs.buildFHSUserEnv {
-      name = "muse-sounds-manager";
-      targetPkgs = pkgs: (with pkgs; [
-        # dotnet
-        curl
-        icu
-        libunwind
-        libuuid
-        openssl
-        zlib
-
-        # mono
-        krb5
-
-        pkgs.my.muse-sounds-deb
-      ]);
-      multiPkgs = pkgs: [pkgs.dpkg];
-      runScript = "/opt/muse-sounds-manager/Muse.Client.Linux";
-    }
-  );
+  muse-sounds-manager = callPackage ./muse-sounds-manager {};
 })
