fdc8a1affd2be3f115805679dd17d4969445424f
Casey Link <unnamedrambler@gmail.com>
2024-02-07 14:23:45 +0100

    start k3s adventure

---
+++ modules/server/k3s/k3s-agent.nix
@@ -0,0 +1,38 @@
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+with lib;
+with lib.my; let
+  cfg = config.modules.agent.k3s-agent;
+  k3s-package = pkgs.unstable.k3s_1_29;
+in {
+  options.modules.agent.k3s-agent = {
+    enable = mkBoolOpt false;
+    tokenFile = mkStrOpt "/etc/k3s-token";
+    serverAddr = mkStrOpt "";
+  };
+  config = let
+    extraFlags = [
+      "--node-label \"k3s-upgrade=false\""
+    ];
+    combinedFlags = concatLists [extraFlags];
+  in
+    mkIf cfg.enable {
+      modules.agent.k3s-common.enable = true;
+      services.k3s = {
+        enable = true;
+        package = k3s-package;
+        role = "agent";
+        serverAddr = cfg.serverAddr;
+        tokenFile = cfg.tokenFile;
+        extraFlags = concatStringsSep " " combinedFlags;
+      };
+
+      environment.systemPackages = [k3s-package];
+    };
+}
---
+++ modules/server/k3s/k3s-common.nix
@@ -0,0 +1,58 @@
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+with lib;
+with lib.my; let
+  cfg = config.modules.agent.k3s-common;
+in {
+  options.modules.agent.k3s-common = {
+    enable = mkBoolOpt false;
+  };
+  config = mkIf cfg.enable {
+    # Based on https://github.com/TUM-DSE/doctor-cluster-config/tree/master/modules/k3s
+    virtualisation.containerd.enable = true;
+    virtualisation.containerd.settings = {
+      version = 2;
+      plugins."io.containerd.grpc.v1.cri" = {
+        cni.conf_dir = "/var/lib/rancher/k3s/agent/etc/cni/net.d/";
+        # FIXME: upstream
+        cni.bin_dir = "${pkgs.runCommand "cni-bin-dir" {} ''
+          mkdir -p $out
+          ln -sf ${pkgs.cni-plugins}/bin/* ${pkgs.cni-plugin-flannel}/bin/* $out
+        ''}";
+      };
+    };
+
+    environment.systemPackages = [
+      (pkgs.writeShellScriptBin "k3s-reset-node" (builtins.readFile ./k3s-reset-node))
+    ];
+    systemd.services.k3s = {
+      wants = ["containerd.service"];
+      after = ["containerd.service"];
+    };
+
+    systemd.services.containerd.serviceConfig = {
+      ExecStartPre = [
+        "-${pkgs.zfs}/bin/zfs create -o mountpoint=/var/lib/containerd/io.containerd.snapshotter.v1.zfs zroot/containerd"
+      ];
+    };
+
+    networking.firewall.allowedTCPPorts = [
+      7472 # MetalLB (TCP+UDP)
+      7473 # MetalLB FRR (TCP+UDP)
+      7946 # MetalLB (TCP+UDP)
+      10250 # K3s Metrics Server (TCP)
+      10254 # MetalLB Metrics Export (TCP)
+    ];
+    networking.firewall.allowedUDPPorts = [
+      7472 # MetalLB (TCP+UDP)
+      7473 # MetalLB FRR (TCP+UDP)
+      7946 # MetalLB (TCP+UDP)
+    ];
+  };
+}
