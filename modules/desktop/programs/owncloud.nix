3591c3082f06b1e6b98b291c03a64155a2c2047b
Casey Link <unnamedrambler@gmail.com>
2024-03-01 16:04:37 +0100

    feat(owncloud): add

---
+++ modules/desktop/programs/owncloud.nix
@@ -0,0 +1,31 @@
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+with lib;
+with lib.my; let
+  cfg = config.modules.desktop.programs.owncloud;
+  username = config.modules.users.primaryUser.username;
+  homeDirectory = config.modules.users.primaryUser.homeDirectory;
+  withImpermanence = config.modules.impermanence.enable;
+in {
+  options.modules.desktop.programs.owncloud = {
+    enable = mkBoolOpt false;
+  };
+  config = mkIf cfg.enable {
+    myhm = {
+      home.packages = [
+        owncloud-client
+      ];
+      persistence = mkIf withImpermanence {
+        directories = [
+          ".config/ownCloud"
+        ];
+      };
+    };
+  };
+}
