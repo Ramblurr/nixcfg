35b3e7dc2c5050ab252477b60e77bed900879974
Casey Link <unnamedrambler@gmail.com>
2024-03-04 10:02:44 +0100

    quine: add fritzing

---
+++ modules/desktop/programs/fritzing.nix
@@ -0,0 +1,35 @@
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+with lib;
+with lib.my; let
+  cfg = config.modules.desktop.programs.fritzing;
+  username = config.modules.users.primaryUser.username;
+  homeDirectory = config.modules.users.primaryUser.homeDirectory;
+  withImpermanence = config.modules.impermanence.enable;
+in {
+  options.modules.desktop.programs.fritzing = {
+    enable = mkBoolOpt false;
+  };
+  config = mkIf cfg.enable {
+    home-manager.users."${username}" = {
+      pkgs,
+      config,
+      ...
+    } @ hm: {
+      home.packages = [
+        pkgs.fritzing
+      ];
+      home.persistence."/persist${homeDirectory}" = mkIf withImpermanence {
+        directories = [
+          ".config/Fritzing"
+        ];
+      };
+    };
+  };
+}
