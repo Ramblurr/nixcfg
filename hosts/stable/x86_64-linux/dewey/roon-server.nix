0e9a95e69856c01a520b9587f40c8237946908b7
Casey Link <unnamedrambler@gmail.com>
2024-03-05 10:47:09 +0100

    run roon-server on bare-metal node

---
+++ hosts/stable/x86_64-linux/dewey/roon-server.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  fetchurl,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: let
+  name = "roon-server";
+  user = "roon-server";
+  group = "roon-server";
+in {
+  services.roon-server = {
+    enable = true;
+    openFirewall = true;
+  };
+  fileSystems."/mnt/roon/backup" = {
+    device = "10.9.10.10:/mnt/tank2/backups/roon";
+    fsType = "nfs";
+  };
+  fileSystems."/mnt/roon/music-other" = {
+    device = "10.9.10.10:/mnt/tank2/media/music/other";
+    fsType = "nfs";
+  };
+  fileSystems."/mnt/roon/music-mine" = {
+    device = "10.9.10.10:/mnt/tank2/media/music/mine";
+    fsType = "nfs";
+  };
+  environment.persistence."/persist" = {
+    hideMounts = true;
+    directories = [
+      "/var/lib/roon-server"
+    ];
+  };
+}
