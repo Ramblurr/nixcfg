435bf5be5fa8b6118f5df69375c16ed39b0eaa9f
Casey Link <unnamedrambler@gmail.com>
2024-03-19 11:33:03 +0100

    quine: add attic client

--- default.nix
+++ default.nix
@@ -31,19 +31,19 @@ with lib.my; {
         "https://hyprland.cachix.org"
         "https://cache.nixos.org"
         "https://nixpkgs-wayland.cachix.org"
-        "https://unmatched.cachix.org"
+        #"https://unmatched.cachix.org"
         "https://nix-community.cachix.org"
         "https://nix-gaming.cachix.org"
-        "https://arm.cachix.org/"
+        #"https://arm.cachix.org/"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-        "unmatched.cachix.org-1:F8TWIP/hA2808FDABsayBCFjrmrz296+5CQaysosTTc="
+        #"unmatched.cachix.org-1:F8TWIP/hA2808FDABsayBCFjrmrz296+5CQaysosTTc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-        "arm.cachix.org-1:5BZ2kjoL1q6nWhlnrbAl+G7ThY7+HaBRD9PZzqZkbnM="
+        #"arm.cachix.org-1:5BZ2kjoL1q6nWhlnrbAl+G7ThY7+HaBRD9PZzqZkbnM="
       ];
       trusted-users = ["root" "@wheel"];
       auto-optimise-store = true;
