96f9ef7f6b1df66ef39b5e1265c12ec75cbc137d
Casey Link <unnamedrambler@gmail.com>
2024-03-19 12:31:13 +0100

    fairybox setup for dev too

--- hosts/stable/aarch64-linux/fairybox/rpi-kernel.nix
+++ hosts/stable/aarch64-linux/fairybox/rpi-kernel.nix
@@ -8,7 +8,6 @@
 }: {
   #boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_rpi4);
   boot.kernelPackages = unstable.linuxPackages_rpi4;
-
   boot.kernelParams = [
     "snd_bcm2835.enable_hdmi=0"
     "iomem=relaxed"
