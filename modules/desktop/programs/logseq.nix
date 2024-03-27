f9a73ad084d3d3edafba2f6393bba80361acd156
Casey Link <unnamedrambler@gmail.com>
2024-03-27 08:36:15 +0100

    logseq: make it work on wayland

--- modules/desktop/programs/logseq.nix
+++ modules/desktop/programs/logseq.nix
@@ -1,37 +1,33 @@
-{
-  options,
-  config,
-  lib,
-  pkgs,
-  inputs,
-  ...
-}:
+{ options, config, lib, pkgs, inputs, ... }:
 with lib;
-with lib.my; let
+with lib.my;
+let
   cfg = config.modules.desktop.programs.logseq;
   username = config.modules.users.primaryUser.username;
   homeDirectory = config.modules.users.primaryUser.homeDirectory;
   withImpermanence = config.modules.impermanence.enable;
 in {
-  options.modules.desktop.programs.logseq = {
-    enable = mkBoolOpt false;
-  };
+  options.modules.desktop.programs.logseq = { enable = mkBoolOpt false; };
   config = mkIf cfg.enable {
-    home-manager.users."${username}" = {
-      pkgs,
-      config,
-      ...
-    } @ hm: {
-      home.packages = [
-        pkgs.logseq
-        #(pkgs.logseq.override {electron_27 = pkgs.electron_28;})
-      ];
-      persistence = mkIf withImpermanence {
-        directories = [
-          ".config/Logseq"
-          ".logseq"
-        ];
+    home-manager.users."${username}" = { pkgs, config, ... }@hm: {
+      home.packages = [ pkgs.logseq ];
+      home.file.".local/share/applications/logseq.desktop" = {
+        text = ''
+          [Desktop Entry]
+          Name=Logseq
+          Exec=env -u NIXOS_OZONE_WL logseq %u
+          Terminal=false
+          Type=Application
+          Icon=logseq
+          StartupWMClass=Logseq
+          X-AppImage-Version=0.10.7
+          Comment=A privacy-first, open-source platform for knowledge management and collaboration.
+          MimeType=x-scheme-handler/logseq
+          Categories=Utility
+        '';
       };
+
+      persistence = mkIf withImpermanence { directories = [ ".config/Logseq" ".logseq" ]; };
     };
   };
 }
