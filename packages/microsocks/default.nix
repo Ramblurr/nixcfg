4c66bd538841209f7a418cc29423791860564a6d
Casey Link <unnamedrambler@gmail.com>
2024-03-27 08:38:37 +0100

    microsocks: update 1.0.4

--- packages/microsocks/default.nix
+++ packages/microsocks/default.nix
@@ -1,20 +1,24 @@
-{
-  stdenv,
-  fetchFromGitHub,
-}:
+{ stdenv, fetchFromGitHub, lib, }:
 stdenv.mkDerivation rec {
   pname = "microsocks";
-  version = "1.0.3";
+  version = "1.0.4";
   src = fetchFromGitHub {
     owner = "rofl0r";
     repo = "microsocks";
     rev = "v${version}";
     fetchSubmodules = true;
-    sha256 = "sha256-l+8+zqeY/Qf5pKvzW9WQBfltEvrD7Vp+hJAXM2o16aI=";
+    sha256 = "sha256-cB2XMWjoZ1zLAmAfl/nqjdOyBDKZ+xtlEmqsZxjnFn0=";
   };
 
   installPhase = ''
-    mkdir -p $out/bin/
-    install -m 755 microsocks -t $out/bin/
+    install -Dm 755 microsocks -t $out/bin/
   '';
+
+  meta = with lib; {
+    homepage = "https://github.com/rofl0r/microsocks";
+    description = "tiny, portable SOCKS5 server with very moderate resource usage ";
+    license = licenses.mit;
+    platforms = platforms.all;
+    #maintainers = with maintainers; [ramblurr];
+  };
 }
