498b7004e7f35b988e89ad18c80e09517225ff04
Casey Link <unnamedrambler@gmail.com>
2024-03-27 14:07:12 +0100

    use upstream nixfmt

--- modules/editors/emacs/default.nix
+++ modules/editors/emacs/default.nix
@@ -1,4 +1,11 @@
-{ options, config, lib, pkgs, inputs, ... }:
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
 with lib;
 with lib.my;
 let
@@ -6,8 +13,11 @@ let
   username = config.modules.users.primaryUser.username;
   homeDirectory = config.modules.users.primaryUser.homeDirectory;
   withImpermanence = config.modules.impermanence.enable;
-in {
-  options.modules.editors.emacs = { enable = mkBoolOpt false; };
+in
+{
+  options.modules.editors.emacs = {
+    enable = mkBoolOpt false;
+  };
   config = mkIf cfg.enable {
     fonts.packages = [ pkgs.emacs-all-the-icons-fonts ];
 
@@ -28,94 +38,113 @@ in {
         ''; # It will always sync when rebuild is done. So changes will always be applied.
       };
     };
-    myhm = { ... }@hm: {
-      programs.emacs = {
-        enable = true;
-        package = pkgs.emacs29;
-        extraPackages = epkgs: [ epkgs.vterm ];
-      };
+    myhm =
+      { ... }@hm:
+      {
+        programs.emacs = {
+          enable = true;
+          package = pkgs.emacs29;
+          extraPackages = epkgs: [ epkgs.vterm ];
+        };
 
-      services.emacs = {
-        enable = true;
-        startWithUserSession = "graphical";
-      };
-      systemd.user.services.emacs = { Service = { TimeoutStartSec = 300; }; };
+        services.emacs = {
+          enable = true;
+          startWithUserSession = "graphical";
+        };
+        systemd.user.services.emacs = {
+          Service = {
+            TimeoutStartSec = 300;
+          };
+        };
 
-      sops.secrets.authinfo = {
-        mode = "0400";
-        path = ".authinfo";
-      };
+        sops.secrets.authinfo = {
+          mode = "0400";
+          path = ".authinfo";
+        };
 
-      home.packages = with pkgs; [
-        ## Doom dependencies
-        git
-        zoxide
-        (ripgrep.override { withPCRE2 = true; })
-        gnutls # for TLS connectivity
-        kitty
+        home.packages = with pkgs; [
+          ## Doom dependencies
+          git
+          zoxide
+          (ripgrep.override { withPCRE2 = true; })
+          gnutls # for TLS connectivity
+          kitty
 
-        ## Optional dependencies
-        dockfmt # docker
-        clang-tools # java format
-        pandoc # markdown
-        shfmt # shell format
-        html-tidy # html format
-        stylelint # css lint
-        nodePackages.js-beautify # js/css/html format
-        nodePackages.prettier
-        fd # faster projectile indexing
-        imagemagick # for image-dired
-        zstd # for undo-fu-session/undo-tree compression
+          ## Optional dependencies
+          dockfmt # docker
+          clang-tools # java format
+          pandoc # markdown
+          shfmt # shell format
+          html-tidy # html format
+          stylelint # css lint
+          nodePackages.js-beautify # js/css/html format
+          nodePackages.prettier
+          fd # faster projectile indexing
+          imagemagick # for image-dired
+          zstd # for undo-fu-session/undo-tree compression
 
-        ## Module dependencies
-        # :checkers spell
-        (aspellWithDicts (ds: with ds; [ en en-computers en-science de ]))
-        hunspell
-        hunspellDicts.en_US
-        hunspellDicts.de_AT
+          ## Module dependencies
+          # :checkers spell
+          (aspellWithDicts (
+            ds: with ds; [
+              en
+              en-computers
+              en-science
+              de
+            ]
+          ))
+          hunspell
+          hunspellDicts.en_US
+          hunspellDicts.de_AT
 
-        # :tools editorconfig
-        editorconfig-core-c # per-project style config
-        # :tools lookup & :lang org +roam
-        sqlite
-        # :lang latex & :lang org (latex previews)
-        texlive.combined.scheme-medium
-        # :lang beancount
-        beancount
-        fava
-        (python311.withPackages (ps: with ps; [ virtualenv black python-lsp-black setuptools ]))
-        # :lang nix
-        nixfmt
-      ];
-      persistence = mkIf withImpermanence { directories = [ ".emacs.d" ]; };
-      home.file.".doom.d" = {
-        # Get Doom Emacs
-        source = ./configs/doom.d; # Sets up symlink name ".doom.d" for file "doom.d"
-        recursive = true; # symlink the whole dirj
-        onChange = builtins.readFile
-          ./configs/doom.sh; # If an edit is detected, it will run this script. Pretty much the same as what is now in default.nix but actually stating the terminal and adding the disown flag to it won't time out
+          # :tools editorconfig
+          editorconfig-core-c # per-project style config
+          # :tools lookup & :lang org +roam
+          sqlite
+          # :lang latex & :lang org (latex previews)
+          texlive.combined.scheme-medium
+          # :lang beancount
+          beancount
+          fava
+          (python311.withPackages (
+            ps: with ps; [
+              virtualenv
+              black
+              python-lsp-black
+              setuptools
+            ]
+          ))
+          # :lang nix
+          inputs.nixfmt.packages.${pkgs.hostPlatform.system}.nixfmt
+        ];
+        persistence = mkIf withImpermanence { directories = [ ".emacs.d" ]; };
+        home.file.".doom.d" = {
+          # Get Doom Emacs
+          source = ./configs/doom.d; # Sets up symlink name ".doom.d" for file "doom.d"
+          recursive = true; # symlink the whole dirj
+          onChange = builtins.readFile ./configs/doom.sh; # If an edit is detected, it will run this script. Pretty much the same as what is now in default.nix but actually stating the terminal and adding the disown flag to it won't time out
+        };
+        home.file.".local/share/icons/doom.png" = {
+          source = ./configs/icons/doom.png;
+          recursive = true;
+        };
+        home.file.".local/share/applications/doom.desktop" = {
+          text = ''
+            [Desktop Entry]
+            Name=Doom
+            GenericName=Text Editor
+            Comment=Edit text
+            MimeType=text/english;text/plain;text/x-makefile;text/x-c++hdr;text/x-c++src;text/x-chdr;text/x-csrc;text/x-java;text/x-moc;text/x-pascal;text/x-tcl;text/x-tex;application/x-shellscript;text/x-c;text/x-c++;text/x-markdown;text/html;application/xhtml+xml
+            Exec=emacsclient -c %F
+            Icon=${hm.config.home.homeDirectory}/.local/share/icons/doom.png
+            Type=Application
+            Terminal=false
+            Categories=Development;TextEditor;
+            StartupWMClass=Doom
+            StartupNotify=true
+            Keywords=Text;Editor;
+          '';
+        };
       };
-      home.file.".local/share/icons/doom.png" = {
-        source = ./configs/icons/doom.png;
-        recursive = true;
-      };
-      home.file.".local/share/applications/doom.desktop" = {
-        text = ''
-          [Desktop Entry]
-          Name=Doom
-          GenericName=Text Editor
-          Comment=Edit text
-          MimeType=text/english;text/plain;text/x-makefile;text/x-c++hdr;text/x-c++src;text/x-chdr;text/x-csrc;text/x-java;text/x-moc;text/x-pascal;text/x-tcl;text/x-tex;application/x-shellscript;text/x-c;text/x-c++;text/x-markdown;text/html;application/xhtml+xml
-          Exec=emacsclient -c %F
-          Icon=${hm.config.home.homeDirectory}/.local/share/icons/doom.png
-          Type=Application
-          Terminal=false
-          Categories=Development;TextEditor;
-          StartupWMClass=Doom
-          StartupNotify=true
-          Keywords=Text;Editor;
-        '';
-      };
-    };
   };
 }
