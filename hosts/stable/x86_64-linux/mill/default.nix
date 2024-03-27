ca5499522f3c8ff13a34d3849721f77203852258
Casey Link <unnamedrambler@gmail.com>
2024-03-27 12:47:44 +0100

    k3s-nodes: use new secret system

--- hosts/stable/x86_64-linux/mill/default.nix
+++ hosts/stable/x86_64-linux/mill/default.nix
@@ -4,16 +4,15 @@ let
   machine-id = "964beb5f91f644d8bea9f74366d1556d";
   defaultSopsFile = ./secrets.sops.yaml;
   ramblurr = import ../../../ramblurr.nix { inherit config lib pkgs inputs; };
-  k3s-main = builtins.fromJSON (builtins.readFile ../../../../secrets/k3s-main.secrets);
 in {
   imports = [ ./hardware.nix ./disk-config.nix ];
   system.stateVersion = "23.11";
   environment.etc."machine-id".text = machine-id;
-
+  repo.secretFiles.k3s-main = ../../../../secrets/k3s-main.nix;
   modules.profiles.k3s-node = {
     enable = true;
     hostname = hn;
-    clusterSettings = k3s-main;
+    clusterSettings = config.repo.secrets.k3s-main;
     user = ramblurr;
     defaultSopsFile = defaultSopsFile;
   };
--- hosts/stable/x86_64-linux/peirce/default.nix
+++ hosts/stable/x86_64-linux/peirce/default.nix
@@ -4,16 +4,16 @@ let
   machine-id = "990ad480d2bf4eacb5d0086ab46ea3f9";
   defaultSopsFile = ./secrets.sops.yaml;
   ramblurr = import ../../../ramblurr.nix { inherit config lib pkgs inputs; };
-  k3s-main = builtins.fromJSON (builtins.readFile ../../../../secrets/k3s-main.secrets);
 in {
   imports = [ ./hardware.nix ./disk-config.nix ];
   system.stateVersion = "23.11";
   environment.etc."machine-id".text = machine-id;
+  repo.secretFiles.k3s-main = ../../../../secrets/k3s-main.nix;
 
   modules.profiles.k3s-node = {
     enable = true;
     hostname = hn;
-    clusterSettings = k3s-main;
+    clusterSettings = config.repo.secrets.k3s-main;
     user = ramblurr;
     defaultSopsFile = defaultSopsFile;
   };
