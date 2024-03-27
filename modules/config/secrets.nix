d95368500586b95caecbc23b1968399ee69b03ca
Casey Link <unnamedrambler@gmail.com>
2024-03-27 12:19:59 +0100

    implement streamlined repository secret handling

---
+++ modules/config/secrets.nix
@@ -0,0 +1,9 @@
+{ config, inputs, lib, ... }:
+
+let local = config.node.secretsDir + "/local.nix";
+in {
+  # Define local repo secrets
+  repo.secretFiles = {
+    global = ../../secrets/global.nix;
+  } // lib.optionalAttrs (lib.pathExists local) { inherit local; };
+}
---
+++ modules/meta.nix
@@ -0,0 +1,8 @@
+{ lib, ... }:
+let inherit (lib) mkOption types;
+in {
+  options.node.secretsDir = mkOption {
+    description = "Path to the secrets directory for this node.";
+    type = types.path;
+  };
+}
