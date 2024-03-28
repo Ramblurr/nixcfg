3c4ba75d2752bc32ade2d6f59866b795f8e98a1d
Casey Link <unnamedrambler@gmail.com>
2024-03-28 16:52:11 +0100

    add my nixpkgs fork to special args

--- lib/nixos.nix
+++ lib/nixos.nix
@@ -1,16 +1,32 @@
-{ inputs, lib, pkgs, ... }:
+{
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
 let
   inherit (lib) mkDefault removeSuffix;
   inherit (lib.my) mapModules;
 
   # nixosSystem: the flake system builder, accessed from the nixpkgs flake using nixpkgs.lib.nixosSystem
-  mkHost = path:
-    { nixosSystem, unstable, system, home-manager, overlays, extraModules, isStable }:
+  mkHost =
+    path:
+    {
+      nixosSystem,
+      unstable,
+      system,
+      home-manager,
+      overlays,
+      extraModules,
+      isStable,
+      mine,
+    }:
     nixosSystem {
       inherit system;
       specialArgs = {
         inherit lib inputs;
         unstable = unstable;
+        mine = mine;
         # this provides modules a way to get a handle on what would normally be `inputs.nixpkgs`
         # but in a way that will ensure they get the input that corresponds to their stable/unstable version.
         actual-nixpkgs = if isStable then inputs.nixpkgs-stable else inputs.nixpkgs-unstable;
@@ -26,4 +42,7 @@ let
         ../. # /default.nix
       ];
     };
-in { mapHosts = dir: attrs: mapModules dir (hostPath: mkHost hostPath attrs); }
+in
+{
+  mapHosts = dir: attrs: mapModules dir (hostPath: mkHost hostPath attrs);
+}
