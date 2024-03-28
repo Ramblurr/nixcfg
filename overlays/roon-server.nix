94d673d260c79478a821d13f5987074d72f7f22c
Casey Link <unnamedrambler@gmail.com>
2024-03-28 16:51:47 +0100

    roon-server: bump

--- overlays/roon-server.nix
+++ overlays/roon-server.nix
@@ -2,7 +2,7 @@ final: prev: {
   roon-server = prev.roon-server.overrideAttrs (old: {
     version = "2.0-1388";
     src = prev.fetchurl {
-      url = "https://download.roonlabs.com/updates/production/RoonServer_linuxx64_200001368.tar.bz2";
+      url = "https://download.roonlabs.com/updates/production/RoonServer_linuxx64_200001388.tar.bz2";
       hash = "sha256-FH5edAtPS7qPtShGz1paEmL1O5xDmCLTRvEWFPiiVjg=";
     };
   });
