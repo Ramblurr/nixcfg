42b1e2f495e21c4018df7adde270f985842597b6
Casey Link <unnamedrambler@gmail.com>
2024-04-08 12:20:53 +0200

    services: redirect http -> https

--- modules/services/paperless.nix
+++ modules/services/paperless.nix
@@ -145,7 +145,7 @@ in
 
     services.nginx.virtualHosts.${cfg.domain} = {
       useACMEHost = cfg.ingress.domain;
-      onlySSL = true;
+      forceSSL = true;
       kTLS = true;
       extraConfig = ''
         client_max_body_size 0;
