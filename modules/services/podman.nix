7b0ee1f471bc0edf03356fc254999db41b52ed0e
Casey Link <unnamedrambler@gmail.com>
2024-04-10 17:04:09 +0200

    podman: add auto update daemon

--- modules/services/podman.nix
+++ modules/services/podman.nix
@@ -27,5 +27,23 @@ in
       defaultNetwork.settings.dns_enabled = true;
       extraPackages = [ pkgs.zfs ];
     };
+
+    systemd.services.podman-auto-update = {
+      wants = [ "network-online.target" ];
+      after = [ "network-online.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.podman}/bin/podman auto-update";
+        ExecStartPost = "${pkgs.podman}/bin/podman image prune -f";
+      };
+    };
+
+    systemd.timers.podman-auto-update = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "03:30";
+        Persistent = true;
+      };
+    };
   };
 }
