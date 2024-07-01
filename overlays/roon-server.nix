33076dee22b7eada81dc1b664ab7efe566140a78
Casey Link <unnamedrambler@gmail.com>
2024-07-01 10:33:02 +0200

    dewey update

--- overlays/roon-server.nix
+++ overlays/roon-server.nix
@@ -1,9 +1,9 @@
 final: prev: {
   roon-server = prev.roon-server.overrideAttrs (old: {
-    version = "2.0-1413";
+    version = "2.0-1432";
     src = prev.fetchurl {
-      url = "https://download.roonlabs.com/updates/production/RoonServer_linuxx64_200001413.tar.bz2";
-      hash = "sha256-VoTJu5+zuFFknDolGJ/69e1i6B4vfR9ev7sAKhfeRlU=";
+      url = "https://download.roonlabs.com/updates/production/RoonServer_linuxx64_200001432.tar.bz2";
+      hash = "sha256-h0Ly5S8ML29RtaZOpe0k4U/R0coClHHGUZyu5d1PqzQ=";
     };
   });
 }
