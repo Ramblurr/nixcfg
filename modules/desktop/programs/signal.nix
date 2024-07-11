f5263f0639797db7269bfdc71820604e8d5952c1
Casey Link <unnamedrambler@gmail.com>
2024-07-11 08:46:45 +0200

    fix signal hidpi on wayland

--- modules/desktop/programs/signal.nix
+++ modules/desktop/programs/signal.nix
@@ -29,6 +29,20 @@ in
       { pkgs, config, ... }@hm:
       {
         home.packages = [ pkgs.signal-desktop ];
+        home.file.".local/share/applications/signal-desktop.desktop" = {
+          text = ''
+            [Desktop Entry]
+            Name=Signal Desktop
+            Exec=/nix/store/pkdmssw4rvnbd55i1x2ha4bx6liv1fdz-signal-desktop-7.14.0/bin/signal-desktop --ozone-platform-hint=auto --no-sandbox %U
+            Terminal=false
+            Type=Application
+            Icon=signal-desktop
+            StartupWMClass=signal
+            Comment=Private messaging from your desktop
+            MimeType=x-scheme-handler/sgnl;x-scheme-handler/signalcaptcha;
+            Categories=Network;InstantMessaging;Chat;
+          '';
+        };
       };
   };
 }
