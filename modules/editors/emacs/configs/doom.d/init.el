a0532a182dfa49ef6aa3bd6f2e44ac22b4083cb1
Casey Link <unnamedrambler@gmail.com>
2024-07-11 08:48:28 +0200

    remove deprecated module

--- modules/editors/emacs/configs/doom.d/init.el
+++ modules/editors/emacs/configs/doom.d/init.el
@@ -100,9 +100,10 @@
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
-       rgb               ; creating color strings
-       ;;taskrunner        ; taskrunner for all your projects
-       terraform         ; infrastructure as code
+       ;; rgb               ; creating color strings
+       ;; ;;taskrunner        ; taskrunner for all your projects
+       ;; terraform
+                                        ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
        tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
