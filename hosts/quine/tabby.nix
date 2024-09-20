e868d29db8e901904ca31bcca8214d28a716b243
Casey Link <unnamedrambler@gmail.com>
2024-09-20 11:25:29 +0200

    switch quine to plasma

--- hosts/quine/tabby.nix
+++ hosts/quine/tabby.nix
@@ -1,14 +1,30 @@
 {
   config,
+  inputs,
   lib,
   pkgs,
+  mine,
   ...
 }:
 
 {
 
-  services.tabby = {
-    enable = true;
-    model = "TabbyML/DeepseekCoder-6.7B";
+  disabledModules = [
+    "${inputs.nixpkgs-unstable}/nixos/modules/services/misc/tabby.nix"
+  ];
+  imports = [
+    "${inputs.nixpkgs-mine}/nixos/modules/services/misc/tabby.nix"
+  ];
+  config = {
+    services.tabby = {
+      enable = true;
+      package = (
+        mine.tabby.override {
+          cudaSupport = true;
+          acceleration = "cuda";
+        }
+      );
+      model = "TabbyML/DeepseekCoder-6.7B";
+    };
   };
 }
