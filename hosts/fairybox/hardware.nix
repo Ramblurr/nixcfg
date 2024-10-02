d8d45cb7c2eb6e02c4b52c8e78fed79062f15ece
Casey Link <unnamedrambler@gmail.com>
2024-10-02 16:52:49 +0200

    nixpkgs update

--- hosts/fairybox/hardware.nix
+++ hosts/fairybox/hardware.nix
@@ -97,7 +97,7 @@
 
   # Trim some fat
   # This causes an overlay which causes a lot of rebuilding
-  environment.noXlibs = lib.mkForce false;
+  #environment.noXlibs = lib.mkForce false;
   # Limit the journal size to X MB or last Y days of logs
   services.journald.extraConfig = ''
     SystemMaxUse=1536M
