971a8775745116e5beb61c0764135fbdd066dd95
Casey Link <unnamedrambler@gmail.com>
2024-10-21 08:31:31 +0200

    Fix kobo e-reader book downloads over calibre-web endpoint

--- modules/services/calibre-web.nix
+++ modules/services/calibre-web.nix
@@ -145,6 +145,9 @@ in
         proxy_buffers 4 256k;
         proxy_buffer_size 256k;
         proxy_busy_buffers_size 256k;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_set_header X-Scheme https
       '';
       extraConfig = ''
         client_max_body_size 0;
