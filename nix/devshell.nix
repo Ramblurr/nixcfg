12ab9c2a62b8e3568fb69374a6227d09266823ab
Me <unnamedrambler@gmail.com>
2025-01-02 11:39:51 +0100

    ensure tools are available in devshell

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -27,6 +27,9 @@
       devshells.default = {
         packages = [
           pkgs.nix # Always use the nix version from this flake's nixpkgs version, so that nix-plugins (below) doesn't fail because of different nix versions.
+          pkgs.rage
+          pkgs.age
+          pkgs.age-plugin-yubikey
         ];
 
         commands = [
