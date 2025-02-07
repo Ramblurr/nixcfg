8473bf525b040b6a92f4d90d4585f5c2e42acc4e
Casey Link <unnamedrambler@gmail.com>
2025-02-07 16:05:42 +0100

    quine: improve local workstation virtd for dev

--- hosts/quine/libvirt.nix
+++ hosts/quine/libvirt.nix
@@ -4,7 +4,55 @@
   pkgs,
   ...
 }:
+let
+
+  username = config.modules.users.primaryUser.username;
+  localZfsStorageXml = pkgs.writeText "zfs-local.xml" ''
+    <pool type='zfs'>
+      <name>zfs-local</name>
+      <uuid>fc626111-d7a2-4c1a-87e3-fb38f3b3f214</uuid>
+      <capacity unit='bytes'>0</capacity>
+      <allocation unit='bytes'>0</allocation>
+      <available unit='bytes'>0</available>
+      <source>
+        <name>rpool/encrypted/safe/vms</name>
+      </source>
+      <target>
+        <path>/dev/zvol/rpool/encrypted/safe/vms</path>
+      </target>
+    </pool>
+  '';
+  vpnVlanNetXml = pkgs.writeText "brvpn70.xml" ''
+    <network>
+      <name>brvpn70</name>
+      <uuid>3e724900-e50e-43ae-ac5e-4ffd90d4ac5f</uuid>
+      <forward mode='bridge'/>
+      <bridge name='brvpn70'/>
+    </network>
+  '';
+
+  primaryVlanNetXml = pkgs.writeText "brprim4.xml" ''
+    <network>
+      <name>brprim4</name>
+      <uuid>6dea79f2-1512-40c6-a2a3-ed0b15a9c72d</uuid>
+      <forward mode='bridge'/>
+      <bridge name='brprim4'/>
+    </network>
+  '';
+in
 {
+
+  systemd.tmpfiles.rules = [
+    "d /persist/var/lib/libvirt 0775 root root -"
+    "d /persist/var/lib/libvirt/storage 0775 root root -"
+    "L+ /persist/var/lib/libvirt/qemu/networks/brvpn70.xml - - - - ${vpnVlanNetXml}"
+    "L+ /persist/var/lib/libvirt/qemu/networks/brprim4.xml - - - - ${primaryVlanNetXml}"
+    "L+ /persist/var/lib/libvirt/storage/zfs-local.xml - - - - ${localZfsStorageXml}"
+  ];
+  environment.persistence."/persist".directories = [ "/var/lib/libvirt" ];
+  environment.persistence."/persist".users.${username}.directories = [
+    ".config/virt-viewer"
+  ];
   virtualisation.libvirtd = {
     enable = true;
     allowedBridges = [
@@ -13,6 +61,11 @@
       "brvpn70"
     ];
   };
+  boot.kernel.sysctl = {
+    "net.ipv4.conf.all.forwarding" = true;
+  };
+
+  boot.kernelModules = [ "vfio-pci" ];
   #services.nfs.server.enable = true;
 
   # Add firewall exception for VirtualBox provider
