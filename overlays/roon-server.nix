49a21342b09297f0ce71eac606fd65ef0819ae49
Casey Link <unnamedrambler@gmail.com>
2025-04-30 14:55:23 +0200

    update roon

--- overlays/roon-server.nix
+++ overlays/roon-server.nix
@@ -3,14 +3,14 @@ final: prev: {
     old:
 
     let
-      version = "2.48.1517";
+      version = "2.50.1528";
       urlVersion = builtins.replaceStrings [ "." ] [ "0" ] version;
     in
     {
       version = version;
       src = prev.fetchurl {
         url = "https://download.roonlabs.com/updates/production/RoonServer_linuxx64_${urlVersion}.tar.bz2";
-        hash = "sha256-2H8lQykhzbHcEW/+Rj+4eQdUMUugUeXivz+/+MEAYxk=";
+        hash = "sha256-8Dxxjj/5JSuXeTxTV2l37ZAmf6BDdhykPSinmgZeEVY=";
       };
     }
   );
