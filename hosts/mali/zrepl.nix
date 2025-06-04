75f00d40ae37a73cebc7fb195decef2064503d43
Casey Link <unnamedrambler@gmail.com>
2025-06-04 16:27:04 +0200

    mali: send replications to rsyncnet

--- hosts/mali/zrepl.nix
+++ hosts/mali/zrepl.nix
@@ -234,7 +234,7 @@ in
               "tank2<" = true;
               "tank2/media<" = false;
               "tank2/media/music/mine" = true;
-              "tank2/replication<" = false;
+              "tank2/replication<" = true;
               "tank2/proxmox<" = false;
               "tank2/backups/gamsjaegers<" = false;
             };
