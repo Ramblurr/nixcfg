e629a415c72853f447c61a7f28e2b46fc660544e
Casey Link <unnamedrambler@gmail.com>
2025-06-06 15:51:50 +0200

    james: add atproto did verification

--- hosts/james/web/personals.nix
+++ hosts/james/web/personals.nix
@@ -8,6 +8,7 @@
 let
 
   inherit (config.repo.secrets.global) domain;
+  inherit (config.repo.secrets.local) atprotoDid;
   work = domain.work;
   domains = [
     domain.personal2
@@ -23,19 +24,25 @@ let
       ];
     };
   };
-  vhostFor = domain: {
-    ${domain} = {
-      serverAliases = [ "www.${domain}" ];
-      useACMEHost = domain;
+  vhostFor = d: {
+    ${d} = {
+      serverAliases = [ "www.${d}" ];
+      useACMEHost = d;
       forceSSL = true;
       kTLS = true;
       http3 = true;
       quic = true;
       globalRedirect = work;
+
+      locations."= /.well-known/atproto-did".extraConfig = lib.mkIf (d == domain.personal2) ''
+        add_header Content-Type text/plain;
+        return 200 '${atprotoDid}';
+      '';
     };
   };
 in
 {
   security.acme.certs = lib.mkMerge (map certFor domains);
   services.nginx.virtualHosts = lib.mkMerge (map vhostFor domains);
+
 }
