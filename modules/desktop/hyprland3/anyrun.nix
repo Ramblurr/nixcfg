9f266c3b1a3d4ab15e56cc0f136a6347badaa965
Casey Link <unnamedrambler@gmail.com>
2025-09-17 15:18:09 +0200

    anyrun use nixpkgs

--- modules/desktop/hyprland3/anyrun.nix
+++ modules/desktop/hyprland3/anyrun.nix
@@ -16,17 +16,14 @@ in
       { ... }@hm:
       {
 
-        #imports = [ inputs.anyrun.homeManagerModules.default ];
         programs.anyrun = {
           enable = lib.mkForce true;
           config = {
             plugins = [
-              # An array of all the plugins you want, which either can be paths to the .so files, or their packages
-              inputs.anyrun.packages.${pkgs.system}.applications
-              inputs.anyrun.packages.${pkgs.system}.rink
-              inputs.anyrun.packages.${pkgs.system}.translate
-              inputs.anyrun.packages.${pkgs.system}.shell
-              inputs.anyrun.packages.${pkgs.system}.symbols
+              "${pkgs.anyrun}/lib/libapplications.so"
+              "${pkgs.anyrun}/lib/libsymbols.so"
+              "${pkgs.anyrun}/lib/libshell.so"
+              "${pkgs.anyrun}/lib/librink.so"
             ];
 
             # x and y are the position of the main box
@@ -92,76 +89,6 @@ in
             }
 
           '';
-          # end extra css
-          extraConfigFiles = {
-            "applications.ron".text = ''
-              Config(
-                // Also show the Desktop Actions defined in the desktop files, e.g. "New Window" from LibreWolf
-                desktop_actions: true,
-                max_entries: 10,
-                // The terminal used for running terminal based desktop entries, if left as `None` a static list of terminals is used
-                // to determine what terminal to use.
-                terminal: Some("foot"),
-              )
-            '';
-            "symbols.ron".text = ''
-              Config(
-                // The prefix that the search needs to begin with to yield symbol results
-                prefix: ":sy",
-
-                // Custom user defined symbols to be included along the unicode symbols
-                symbols: {
-                  // "name": "text to be copied"
-                  "shrug": "¯\\_(ツ)_/¯",
-                },
-
-                // The number of entries to be displayed
-                max_entries: 5,
-              )
-            '';
-
-            "translate.ron".text = ''
-              Config(
-                prefix: ":tr",
-                language_delimiter: ">",
-                max_entries: 3,
-              )
-            '';
-            "dictionary.ron".text = ''
-              Config(
-                prefix: ":d",
-                max_entries: 5,
-              )
-            '';
-
-            "shell.ron".text = ''
-              Config(
-                prefix: ":sh",
-                shell: "zsh",
-                max_entries: 5,
-              )
-            '';
-            #"nixos-options.ron".text =
-            #  let
-            #    nixos-options = osConfig.system.build.manual.optionsJSON + "/share/doc/nixos/options.json";
-            #    hm-options = inputs.home-manager.packages.docs-json + "/share/doc/home-manager/options.json";
-            #    options = builtins.toJSON {
-            #      ":nix" = [ nixos-options ];
-            #      ":hm" = [ hm-options ];
-            #      ":nall" = [
-            #        nixos-options
-            #        hm-options
-            #      ];
-            #    };
-            #  in
-            #  ''
-            #    Config(
-            #      options: ${options},
-            #      min_score: 5,
-            #      max_entries: Some(3),
-            #    )
-            #  '';
-          };
         };
       };
   };
