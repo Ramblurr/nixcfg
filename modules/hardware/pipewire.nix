313ebab8022a31c30f8d86edfbaadddaa04fbc82
Casey Link <unnamedrambler@gmail.com>
2025-09-30 12:33:28 +0200

    Switch from rnnoise to deepfilternet2

--- modules/hardware/pipewire.nix
+++ modules/hardware/pipewire.nix
@@ -11,47 +11,6 @@ let
   cfg = config.modules.hardware.pipewire;
   username = config.modules.users.primaryUser.username;
   withImpermanence = config.modules.impermanence.enable;
-
-  json = pkgs.formats.json { };
-
-  pw_rnnoise_config = {
-    "context.modules" = [
-      {
-        "name" = "libpipewire-module-filter-chain";
-        "args" = {
-          "node.description" = "Noise Canceling source";
-          "media.name" = "Noise Canceling source";
-          "filter.graph" = {
-            "nodes" = [
-              {
-                "type" = "ladspa";
-                "name" = "rnnoise";
-                "plugin" = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
-                "label" = "noise_suppressor_stereo";
-                "control" = {
-                  "VAD Threshold (%)" = 95.0;
-                  "VAD Grace Period (ms)" = 200;
-                  "Retroactive VAD Grace (ms)" = 0;
-                };
-              }
-            ];
-          };
-          "audio.position" = [
-            "FL"
-            "FR"
-          ];
-          "capture.props" = {
-            "node.name" = "effect_input.rnnoise";
-            "node.passive" = true;
-          };
-          "playback.props" = {
-            "node.name" = "effect_output.rnnoise";
-            "media.class" = "Audio/Source";
-          };
-        };
-      }
-    ];
-  };
 in
 {
   imports = [ ];
@@ -68,25 +27,63 @@ in
       pulseaudio
       pulsemixer
       libcamera
-      #pw-viz # TODO(disabled 2023-12-01 compilation error) https://github.com/NixOS/nixpkgs/issues/268450
+      pw-viz
     ];
     programs.dconf.enable = true;
     systemd.user.services.pipewire-pulse.path = [ pkgs.pulseaudio ];
     services.pipewire = {
       enable = true;
       alsa.enable = true;
-      alsa.support32Bit = false; # ? lets see if this breaks something
-      pulse.enable = true;
-      jack.enable = false;
-      wireplumber.enable = true;
       audio.enable = true;
+      jack.enable = false;
+      pulse.enable = true;
+      wireplumber.enable = true;
     };
 
     home-manager.users."${username}" =
       { pkgs, ... }@hm:
       mkIf cfg.denoise.enable {
-        xdg.configFile."pipewire/pipewire.conf.d/99-input-denoising.conf" = {
-          source = json.generate "99-input-denoising.conf" pw_rnnoise_config;
+        xdg.configFile."pipewire/pipewire.conf.d/99-deepfilternet.conf" = {
+          text = builtins.toJSON {
+            "context.properties" = {
+              "link.max-buffers" = 16;
+              "core.daemon" = true;
+              "core.name" = "pipewire-0";
+              "module.x11.bell" = false;
+              "module.access" = true;
+              "module.jackdbus-detect" = false;
+            };
+
+            "context.modules" = [
+              {
+                name = "libpipewire-module-filter-chain";
+                args = {
+                  "node.description" = "DeepFilter Noise Canceling source";
+                  "media.name" = "DeepFilter Noise Canceling source";
+
+                  "filter.graph" = {
+                    nodes = [
+                      {
+                        type = "ladspa";
+                        name = "DeepFilter Mono";
+                        plugin = "${pkgs.deepfilternet}/lib/ladspa/libdeep_filter_ladspa.so";
+                        label = "deep_filter_mono";
+                        control = {
+                          "Attenuation Limit (dB)" = 100;
+                        };
+                      }
+                    ];
+                  };
+
+                  "audio.rate" = 48000;
+                  "audio.position" = "[MONO]";
+
+                  "capture.props"."node.passive" = true;
+                  "playback.props"."media.class" = "Audio/Source";
+                };
+              }
+            ];
+          };
         };
       };
   };
