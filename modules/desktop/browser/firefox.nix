ed68510d773602e18b1405695ab74babb00c4298
Casey Link <unnamedrambler@gmail.com>
2025-10-01 12:49:43 +0200

    firefox: remove old mutli profile desktop entries and gnome theme

--- modules/desktop/browser/firefox.nix
+++ modules/desktop/browser/firefox.nix
@@ -48,20 +48,7 @@ in
             path = "personal";
             settings = {
               "browser.tabs.loadInBackground" = true;
-              "widget.gtk.rounded-bottom-corners.enabled" = true;
-              "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-              "svg.context-properties.content.enabled" = true;
-              "gnomeTheme.hideSingleTab" = true;
-              "gnomeTheme.bookmarksToolbarUnderTabs" = true;
-              "gnomeTheme.normalWidthTabs" = false;
-              "gnomeTheme.tabsAsHeaderbar" = false;
             };
-            userChrome = ''
-              @import "firefox-gnome-theme/userChrome.css";
-            '';
-            userContent = ''
-              @import "firefox-gnome-theme/userContent.css";
-            '';
           };
 
           profiles.work = {
@@ -78,51 +65,6 @@ in
             };
           };
         };
-
-        home.file."firefox-gnome-theme" = {
-          target = ".mozilla/firefox/default/chrome/firefox-gnome-theme";
-          source = inputs.firefox-gnome-theme;
-        };
-        # home.file.".local/share/applications/firefox-work.desktop" = {
-        #   text = ''
-        #     [Desktop Entry]
-        #     Actions=new-private-window
-        #     Categories=Network;WebBrowser
-        #     Exec=${firefox-nightly}/bin/firefox-nightly -P work --name firefox-work --class=firefox-work %U
-        #     GenericName=Work Web Browser
-        #     Icon=firefox-nightly
-        #     MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xml;x-scheme-handler/http;x-scheme-handler/https
-        #     Name=Work Firefox
-        #     StartupNotify=true
-        #     StartupWMClass=firefox-work
-        #     Terminal=false
-        #     Type=Application
-        #     Version=1.4
-        #     [Desktop Action new-private-window]
-        #     Exec=${firefox-nightly}/bin/firefox-nightly -P work --name firefox-work --class=firefox-work --private-window %U
-        #     Name=New Work Private Window
-        #   '';
-        # };
-        # home.file.".local/share/applications/firefox-personal.desktop" = {
-        #   text = ''
-        #     [Desktop Entry]
-        #     Actions=new-private-window
-        #     Categories=Network;WebBrowser
-        #     Exec=${firefox-nightly}/bin/firefox-nightly -P personal --name firefox-personal --class=firefox-personal %U
-        #     GenericName=Personal Web Browser
-        #     Icon=firefox-nightly
-        #     MimeType=text/html;text/xml;application/xhtml+xml;application/vnd.mozilla.xul+xml;x-scheme-handler/http;x-scheme-handler/https
-        #     Name=Personal Firefox
-        #     StartupNotify=true
-        #     StartupWMClass=firefox-personal
-        #     Terminal=false
-        #     Type=Application
-        #     Version=1.4
-        #     [Desktop Action new-private-window]
-        #     Exec=${firefox-nightly}/bin/firefox-nightly -P personal --name firefox-personal --class=firefox-personal --private-window %U
-        #     Name=New Personal Private Window
-        #   '';
-        # };
       };
   };
 }
