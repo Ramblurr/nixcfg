b1d236a0cb27925f7d769384ec7ccc6396caa02e
Casey Link <unnamedrambler@gmail.com>
2025-10-01 12:05:25 +0200

    Add helpful, easy to remember, audio/mic commands

--- modules/hardware/pipewire.nix
+++ modules/hardware/pipewire.nix
@@ -6,19 +6,52 @@
   inputs,
   ...
 }:
-with lib;
 let
   cfg = config.modules.hardware.pipewire;
   username = config.modules.users.primaryUser.username;
   withImpermanence = config.modules.impermanence.enable;
+
+  speaker-mute = pkgs.writeShellScriptBin "speaker-mute" ''
+    #!${pkgs.runtimeShell}
+    ${pkgs.alsa-utils}/bin/amixer -D pipewire set Master mute
+  '';
+  mic-mute = pkgs.writeShellScriptBin "mic-mute" ''
+    #!${pkgs.runtimeShell}
+    ${pkgs.alsa-utils}/bin/amixer -D pipewire set Capture nocap
+  '';
+  speaker-unmute = pkgs.writeShellScriptBin "speaker-unmute" ''
+    #!${pkgs.runtimeShell}
+    ${pkgs.alsa-utils}/bin/amixer -D pipewire set Master unmute
+  '';
+  mic-unmute = pkgs.writeShellScriptBin "mic-unmute" ''
+    #!${pkgs.runtimeShell}
+    ${pkgs.alsa-utils}/bin/amixer -D pipewire set Capture cap
+  '';
+
+  speaker-get-mute = pkgs.writeShellScriptBin "speaker-get-mute" ''
+    #!${pkgs.runtimeShell}
+    if ${pkgs.alsa-utils}/bin/amixer -D pipewire get Master | grep -q '\[off\]'; then
+      echo "1"
+    else
+      echo "0"
+    fi
+  '';
+
+  mic-get-mute = pkgs.writeShellScriptBin "mic-get-mute" ''
+    #!${pkgs.runtimeShell}
+    if ${pkgs.alsa-utils}/bin/amixer -D pipewire get Capture | grep -q '\[off\]'; then
+      echo "1"
+    else
+      echo "0"
+    fi
+  '';
 in
 {
-  imports = [ ];
   options.modules.hardware.pipewire = {
     enable = lib.mkEnableOption "";
     denoise.enable = lib.mkEnableOption "";
   };
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     security.rtkit.enable = true;
     environment.systemPackages = with pkgs; [
       alsa-utils # ignore for now cross-compile problem
@@ -39,11 +72,19 @@ in
       pulse.enable = true;
       wireplumber.enable = true;
     };
-
     home-manager.users."${username}" =
       { pkgs, ... }@hm:
-      mkIf cfg.denoise.enable {
-        xdg.configFile."pipewire/pipewire.conf.d/99-deepfilternet.conf" = {
+      {
+        home.packages = [
+          speaker-mute
+          speaker-unmute
+          speaker-get-mute
+          mic-mute
+          mic-unmute
+          mic-get-mute
+        ];
+
+        xdg.configFile."pipewire/pipewire.conf.d/99-deepfilternet.conf" = lib.mkIf cfg.denoise.enable {
           text = builtins.toJSON {
             "context.properties" = {
               "link.max-buffers" = 16;
