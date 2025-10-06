be26ee2f8a08a85e85b9a71b2fa041ea21b07d38
Casey Link <unnamedrambler@gmail.com>
2025-10-06 10:08:17 +0200

    Refactor greetd into standalone module

--- modules/desktop/hyprland3/default.nix
+++ modules/desktop/hyprland3/default.nix
@@ -58,12 +58,6 @@ in
       #};
     };
 
-    security = {
-      polkit.enable = true;
-      # unlock keyring on login
-      pam.services.greetd.enableGnomeKeyring = true;
-    };
-
     environment.systemPackages =
       with pkgs;
       #with gnome;
@@ -121,61 +115,6 @@ in
         gnome-keyring.enable = true;
         gnome-online-accounts.enable = true;
       };
-      #displayManager.enable = false;
-      #displayManager.sddm.wayland.enable = false;
-      #displayManager.sddm.enable = false;
-      #displayManager.ly.enable = false;
-      greetd = {
-        enable = true;
-        settings = rec {
-          terminal = {
-            vt = lib.mkForce 7;
-          };
-          tuigreet_session =
-            let
-              session = "${pkgs.hyprland}/bin/Hyprland";
-              tuigreet = "${lib.getExe pkgs.tuigreet}";
-              opts = [
-                "--time"
-                "--remember"
-                "--theme"
-                "border=lightblue;prompt=green;time=orange;button=yellow;container=black"
-                "--power-shutdown"
-                "systemctl poweroff -i"
-                "--power-reboot"
-                "systemctl reboot -i"
-                "--cmd"
-                "${session}"
-              ];
-            in
-            {
-              command = "${tuigreet} ${lib.strings.escapeShellArgs opts}";
-              user = "greeter";
-            };
-          default_session = tuigreet_session;
-        };
-      };
     };
-    systemd.tmpfiles.rules = lib.mkIf withImpermanence [
-      "d /persist/var/cache/greeter - greeter greeter - -"
-    ];
-    environment.persistence."/persist".directories = lib.mkIf withImpermanence [ "/var/cache/greeter" ];
-    # allow greeter to poweroff and reboot
-    security.polkit.extraConfig = ''
-       polkit.addRule(function(action, subject) {
-      if (
-        subject.user == "greeter"
-          && (
-            action.id == "org.freedesktop.login1.reboot" ||
-            action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
-            action.id == "org.freedesktop.login1.power-off" ||
-            action.id == "org.freedesktop.login1.power-off-multiple-sessions"
-          )
-        )
-      {
-        return polkit.Result.YES;
-      }
-       })
-    '';
   };
 }
