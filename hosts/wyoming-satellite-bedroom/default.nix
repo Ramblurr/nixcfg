59f88ace14bbcd4208700d9f5a32fa70c1a44761
Casey Link <unnamedrambler@gmail.com>
2025-12-28 09:50:34 +0100

    rpi and wyoming satellite tweaks

--- hosts/wyoming-satellite-bedroom/default.nix
+++ hosts/wyoming-satellite-bedroom/default.nix
@@ -32,16 +32,9 @@
     users.enable = true;
     users.userborn.enable = false;
     users.headless.enable = true;
-    users.primaryUser.extraGroups = [
-      "wheel"
-    ];
-    #users.primaryUser.password.enable = false;
-    hardware = {
-      fwupd.enable = false;
-      udisks2.enable = false;
-      rpi.type = "rpi4";
-      rpi.hifiberry-dac-plus.enable = true;
-    };
+    users.primaryUser.extraGroups = [ "wheel" ];
+    hardware.fwupd.enable = false;
+    hardware.udisks2.enable = false;
   };
   home.wifi.iot.enable = true;
   services.tailscale.enable = true;
@@ -49,11 +42,26 @@
     useDHCP = true;
     interfaces.eth0.useDHCP = true;
   };
-
+  environment.systemPackages = [
+    pkgs.pipewire
+    pkgs.alsa-utils
+  ];
+  # this is a system wide pipewire config, users have to be in the pipewire group
+  users.users.ramblurr.extraGroups = [ "pipewire" ];
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    audio.enable = true;
+    systemWide = true;
+    wireplumber.enable = true;
+  };
+  networking.firewall.allowedTCPPorts = [
+    10700 # wyoming-satellite
+  ];
   services.wyoming.satellite = {
     enable = true;
     user = "ramblurr";
-    group = "audio";
+    group = "pipewire";
     uri = "tcp://0.0.0.0:10700";
     name = "bedroom-announce-satellite";
     area = "bedroom";
@@ -62,22 +70,17 @@
     };
     vad.enable = false;
   };
-  systemd.services."wyoming-satellite".path = [
-    pkgs.pipewire
-    pkgs.alsa-utils
-  ];
 
-  environment.systemPackages = [
-    pkgs.pipewire
-    pkgs.alsa-utils
-  ];
-  # use system wide pipewire
-  users.users.ramblurr.extraGroups = [ "pipewire" ];
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    audio.enable = true;
-    systemWide = true;
-    wireplumber.enable = true;
+  systemd.services."wyoming-satellite" = {
+    serviceConfig = {
+      RestartSec = "1";
+      Restart = "always";
+      RuntimeMaxSec = "7200";
+    };
+    path = [
+      pkgs.pipewire
+      pkgs.pipewire
+      pkgs.alsa-utils
+    ];
   };
 }
