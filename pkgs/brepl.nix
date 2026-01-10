ec51a0b93772aaaef2a2c367065020c068bcba86
Casey Link <unnamedrambler@gmail.com>
2026-01-10 07:43:38 +0100

    update brepl

--- pkgs/brepl.nix
+++ pkgs/brepl.nix
@@ -7,8 +7,8 @@ callPackage (
   fetchFromGitHub {
     owner = "licht1stein";
     repo = "brepl";
-    rev = "v2.3.1";
-    hash = "sha256-zNzQ2JTYWwaWJ5inJa2B8WHu3CFM8CzqkWuB/Ekr7lw=";
+    rev = "v2.5.1";
+    hash = "sha256-5rv7fFRe32a1JL2BAD3mMP2b3VGtiMC+MDphhJgJkgk=";
   }
   + "/package.nix"
 ) { }
