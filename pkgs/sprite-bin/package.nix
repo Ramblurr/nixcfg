bf25d165a6c087687de71f4b186979ddd33cec0d
Casey Link <unnamedrambler@gmail.com>
2026-01-11 12:00:29 +0100

    sprite-bin update

--- pkgs/sprite-bin/package.nix
+++ pkgs/sprite-bin/package.nix
@@ -11,13 +11,13 @@ let
   version = "0.0.1-rc29";
 
   sources = {
-    aarch64-darwin.url = "https://sprites-binaries.t3.storage.dev/client/v0.0.1-rc29/sprite-darwin-arm64.tar.gz";
+    aarch64-darwin.url = "https://sprites-binaries.t3.storage.dev/client/v${version}/sprite-darwin-arm64.tar.gz";
     aarch64-darwin.hash = "sha256-HyEHGQF0S9BaKp80jZ8NzcwgtUFGrmycs5cJbCJJTs8=";
-    x86_64-darwin.url = "https://sprites-binaries.t3.storage.dev/client/v0.0.1-rc29/sprite-darwin-amd64.tar.gz";
+    x86_64-darwin.url = "https://sprites-binaries.t3.storage.dev/client/v${version}/sprite-darwin-amd64.tar.gz";
     x86_64-darwin.hash = "sha256-dJLI0Cn+8MhOtf7Id6/Pnlxz1+GYYhTsQI56TgBmE6U=";
-    aarch64-linux.url = "https://sprites-binaries.t3.storage.dev/client/v0.0.1-rc29/sprite-linux-arm64.tar.gz";
+    aarch64-linux.url = "https://sprites-binaries.t3.storage.dev/client/v${version}/sprite-linux-arm64.tar.gz";
     aarch64-linux.hash = "sha256-PECd9HjRFNLMfVBEvNZhyU1JsRdwOY2hmaOdyq4Qd9Y=";
-    x86_64-linux.url = "https://sprites-binaries.t3.storage.dev/client/v0.0.1-rc29/sprite-linux-amd64.tar.gz";
+    x86_64-linux.url = "https://sprites-binaries.t3.storage.dev/client/v${version}/sprite-linux-amd64.tar.gz";
     x86_64-linux.hash = "sha256-4AVjv/ZWM4QYmOJFz9/ky1w3cO9Vc93Tq0Voa2dRfP4=";
   };
 
