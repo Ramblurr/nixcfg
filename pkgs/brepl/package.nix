760cb818fe64a38b22eb95226e5151d78a7aadf0
Casey Link <unnamedrambler@gmail.com>
2026-01-11 12:15:46 +0100

    add brepl auto update

---
+++ pkgs/brepl/package.nix
@@ -0,0 +1,33 @@
+{
+  callPackage,
+  fetchFromGitHub,
+  nix-prefetch-github,
+  ast-grep,
+  gh,
+  nushellPlugins,
+  pkgs-lib,
+  ...
+}:
+let
+  src = fetchFromGitHub {
+    owner = "licht1stein";
+    repo = "brepl";
+    rev = "v2.5.1";
+    hash = "sha256-5rv7fFRe32a1JL2BAD3mMP2b3VGtiMC+MDphhJgJkgk=";
+  };
+  pkg = callPackage (src + "/package.nix") { };
+in
+pkg.overrideAttrs (old: {
+  passthru = (old.passthru or { }) // {
+    updateScript = pkgs-lib.writeUpdateScript {
+      packageToUpdate = "brepl";
+      utils = [
+        nix-prefetch-github
+        ast-grep
+        gh
+      ];
+      nushellPlugins = [ ];
+      script = ./update.nu;
+    };
+  };
+})
