15530b8452a798e004fdc22ae8ab0e382e36af35
Casey Link <14830+Ramblurr@users.noreply.github.com>
2026-01-17 02:41:50 +0000

    update(pkgs): Update sources of all downstream packages

--- pkgs/brepl/package.nix
+++ pkgs/brepl/package.nix
@@ -11,8 +11,8 @@ let
   src = fetchFromGitHub {
     owner = "licht1stein";
     repo = "brepl";
-    rev = "v2.5.1";
-    hash = "sha256-5rv7fFRe32a1JL2BAD3mMP2b3VGtiMC+MDphhJgJkgk=";
+    rev = "";
+    hash = "";
   };
   pkg = callPackage (src + "/package.nix") { };
 in
