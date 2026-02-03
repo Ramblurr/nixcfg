2e69522419ed8841e2f4705e31804fb359238b5f
Casey Link <unnamedrambler@gmail.com>
2026-02-03 09:13:23 +0100

    add authentik terraform

---
+++ terraform/authentik/flows.tf
@@ -0,0 +1,84 @@
+resource "authentik_flow" "provider-authorization-implicit-consent" {
+  name               = "Authorize Application"
+  title              = "Redirecting to %(app)s"
+  slug               = "provider-authorization-implicit-consent"
+  policy_engine_mode = "any"
+  denied_action      = "message_continue"
+  designation        = "authorization"
+  # background         = "https://placeholder.jpeg"
+}
+
+data "authentik_flow" "default-authentication-flow" {
+  slug = "default-authentication-flow"
+}
+
+data "authentik_flow" "default-invalidation-flow" {
+  slug = "default-invalidation-flow"
+}
+
+data "authentik_flow" "default-provider-invalidation-flow" {
+  slug = "default-provider-invalidation-flow"
+}
+
+data "authentik_flow" "default-provider-authorization-explicit-consent" {
+  slug = "default-provider-authorization-explicit-consent"
+}
+
+
+# Recovery flow
+resource "authentik_flow" "recovery" {
+  name               = "recovery-flow"
+  title              = "Password recovery"
+  slug               = "password-recovery"
+  designation        = "recovery"
+  compatibility_mode = true
+  # background         = "https://placeholder.jpeg"
+}
+
+resource "authentik_flow_stage_binding" "recovery-flow-binding-00" {
+  target = authentik_flow.recovery.uuid
+  stage  = authentik_stage_identification.recovery-identification.id
+  order  = 0
+}
+
+#resource "authentik_flow_stage_binding" "recovery-flow-binding-10" {
+#  target = authentik_flow.recovery.uuid
+#  stage  = authentik_stage_email.recovery-email.id
+#  order  = 10
+#}
+
+resource "authentik_flow_stage_binding" "recovery-flow-binding-20" {
+  target = authentik_flow.recovery.uuid
+  stage  = authentik_stage_prompt.password-change-prompt.id
+  order  = 20
+}
+
+resource "authentik_flow_stage_binding" "recovery-flow-binding-30" {
+  target = authentik_flow.recovery.uuid
+  stage  = authentik_stage_user_write.password-change-write.id
+  order  = 30
+}
+
+
+## User settings flow
+resource "authentik_flow" "user-settings" {
+  name               = "User settings"
+  title              = "Update your info"
+  slug               = "user-settings-flow"
+  policy_engine_mode = "any"
+  denied_action      = "message_continue"
+  designation        = "stage_configuration"
+  # background         = "https://placeholder.jpeg"
+}
+
+resource "authentik_flow_stage_binding" "user-settings-flow-binding-20" {
+  target = authentik_flow.user-settings.uuid
+  stage  = authentik_stage_prompt.user-settings.id
+  order  = 20
+}
+
+resource "authentik_flow_stage_binding" "user-settings-flow-binding-100" {
+  target = authentik_flow.user-settings.uuid
+  stage  = authentik_stage_user_write.user-settings-write.id
+  order  = 100
+}
---
+++ terraform/authentik/scopes.tf
@@ -0,0 +1,13 @@
+data "authentik_property_mapping_provider_scope" "oauth2" {
+  managed_list = [
+    "goauthentik.io/providers/oauth2/scope-openid",
+    "goauthentik.io/providers/oauth2/scope-email",
+    "goauthentik.io/providers/oauth2/scope-profile"
+  ]
+}
+
+data "authentik_property_mapping_provider_scope" "oauth2_offline" {
+  managed_list = [
+    "goauthentik.io/providers/oauth2/scope-offline_access"
+  ]
+}
