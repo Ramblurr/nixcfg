eebf7b09d3b6862cd1b2dd7c3eac0994f1ab3d45
Casey Link <unnamedrambler@gmail.com>
2026-02-27 10:49:44 +0100

    update(pkgs): Update sources of all downstream packages

--- pkgs/nvidia/package.nix
+++ pkgs/nvidia/package.nix
@@ -8,9 +8,9 @@ let
   inherit (inputs.self.nixosConfigurations.quine.config.boot) kernelPackages;
 in
 (kernelPackages.nvidiaPackages.mkDriver {
-  version = "580.119.02";
-  sha256_64bit = "sha256-gCD139PuiK7no4mQ0MPSr+VHUemhcLqerdfqZwE47Nc=";
-  openSha256 = "sha256-l3IQDoopOt0n0+Ig+Ee3AOcFCGJXhbH1Q1nh1TEAHTE=";
+  version = "580.126.18";
+  sha256_64bit = "sha256-p3gbLhwtZcZYCRTHbnntRU0ClF34RxHAMwcKCSqatJ0=";
+  openSha256 = "sha256-1Q2wuDdZ6KiA/2L3IDN4WXF8t63V/4+JfrFeADI1Cjg=";
 
   useSettings = false;
   usePersistenced = false;
