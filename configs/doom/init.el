66ead67d1ab08b95fb84bf7775ef1dfbb637881f
Casey Link <unnamedrambler@gmail.com>
2026-03-20 08:49:31 +0100

    i do kotlin now

--- configs/doom/init.el
+++ configs/doom/init.el
@@ -158,7 +158,7 @@
            (java +lsp)       ; the poster child for carpal tunnel syndrome
            (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
            ;;julia             ; a better, faster MATLAB
-           ;;kotlin            ; a better, slicker Java(Script)
+           kotlin            ; a better, slicker Java(Script)
            ;;latex             ; writing papers in Emacs has never been so fun
            ;;lean              ; for folks with too much to prove
            ;;ledger            ; be audit you can be
