96f502ca8c3d43b947be7f54ea53a8c878357504
Casey Link <unnamedrambler@gmail.com>
2026-04-08 11:59:50 +0200

    quine: disable attic-watch-store due to attic issue #226
    Ref: https://github.com/zhaofengli/attic/issues/226

--- hosts/quine/default.nix
+++ hosts/quine/default.nix
@@ -226,7 +226,8 @@ in
     };
     services = {
       docker.enable = true;
-      attic-watch-store.enable = true;
+      # ref: https://github.com/zhaofengli/attic/issues/226
+      attic-watch-store.enable = false;
       docker.enableOnBoot = false;
       podman.enable = true;
       nomad.enable = false;
