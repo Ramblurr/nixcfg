a282746e6888852abcb85a2677e06687416069fe
Casey Link <unnamedrambler@gmail.com>
2026-04-09 12:02:26 +0200

    stop using pinned authentik

--- overlays/last-known-good.nix
+++ overlays/last-known-good.nix
@@ -15,19 +15,10 @@ let
     )) { inherit (prev.stdenv.hostPlatform) system; };
 in
 {
-  inherit
-    (nixpkgs {
-      rev = "5e35f8875cfa5871cf7af29575dd9186b615a314";
-      sha256 = "sha256-WOxcqjpSEh8tUGUNJ6CSal4qGCDWruKhBPvPF0sZYcg=";
-    })
-    authentik
-    ;
-
   #inherit
   #  (nixpkgs {
   #    rev = "5e35f8875cfa5871cf7af29575dd9186b615a314";
-  #    sha256 = "3d1ccb8d3095393e361d10207188037b592e3d91";
-  #    config = final.config;
+  #    sha256 = "sha256-WOxcqjpSEh8tUGUNJ6CSal4qGCDWruKhBPvPF0sZYcg=";
   #  })
   #  authentik
   #  ;
