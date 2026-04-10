f609f53ce4653a8db62c7858b58535c61d8a141b
Casey Link <unnamedrambler@gmail.com>
2026-04-10 09:04:59 +0200

    migrateo to pq safe disaster recovery coldkey

--- hosts/mali/ups.nix
+++ hosts/mali/ups.nix
@@ -86,26 +86,11 @@
       };
     };
   };
-
-  #sops.secrets.upsd-users = {
-  #  format = "binary";
-  #  sopsFile = ./upsd.sops.users;
-  #};
-  #sops.secrets.upsmon-conf = {
-  #  format = "binary";
-  #  sopsFile = ./upsmon.sops.conf;
-  #};
   sops.secrets.upsAdminPassword = {
     mode = "0440";
     group = "nut";
   };
-  #environment.etc."nut/upsd.users".source = config.sops.secrets.upsd-users.path;
-  #environment.etc."nut/upsmon.conf".source = config.sops.secrets.upsmon-conf.path;
-  #environment.etc."nut/upsd.conf".text = ''
-  #  LISTEN 127.0.0.1 3493
-  #'';
   users.users.nut = {
-    #uid = 84;
     isSystemUser = true;
     home = "/var/lib/nut";
     createHome = true;
@@ -113,7 +98,6 @@
     description = "UPS monitor user";
   };
   users.groups."nut" = {
-    #gid = 84;
   };
   systemd.services.upsd.serviceConfig = {
     User = "root";
