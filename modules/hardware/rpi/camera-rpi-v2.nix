6863b635be40ec6628bd463e2652a9ac832922d8
Casey Link <unnamedrambler@gmail.com>
2026-04-20 09:22:11 +0200

    also add camera rpi v2 module

---
+++ modules/hardware/rpi/camera-rpi-v2.nix
@@ -0,0 +1,35 @@
+# Raspberry Pi V2 camera module (OV5647)
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.modules.hardware.rpi.camera-rpi-v2;
+  inherit (config.boot.kernelPackages) kernel;
+in
+
+{
+  options = {
+    modules.hardware.rpi.camera-rpi-v2 = {
+      enable = lib.mkEnableOption ''
+        support for the Raspberry Pi Camera v2
+      '';
+
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    hardware.deviceTree = {
+      enable = true;
+      overlays = [
+        {
+          name = "imx219.dtbo";
+          dtsFile = lib.my.mkCompatibleDtsFile "${kernel}/dtbs/overlays/imx219.dtbo";
+        }
+      ];
+    };
+    modules.hardware.rpi.deviceTree.enable = true;
+  };
+}
