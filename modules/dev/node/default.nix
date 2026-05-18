01fe273dd08b578cc71ac7ba0970aff1fedc0865
Casey Link <unnamedrambler@gmail.com>
2026-05-18 09:22:31 +0200

    disable npm for node dev module

--- modules/dev/node/default.nix
+++ modules/dev/node/default.nix
@@ -4,30 +4,33 @@
   pkgs,
   ...
 }:
-with lib;
 let
   devCfg = config.modules.dev;
   cfg = devCfg.node;
   inherit (config.modules.users.primaryUser) username;
   withImpermanence = config.modules.impermanence.enable;
+  nodejs = pkgs.nodejs-slim_24;
+  pnpm = pkgs.pnpm.override { inherit nodejs; };
+  yarn = pkgs.yarn.override { inherit nodejs; };
+  npmUnavailable = pkgs.writeShellScriptBin "npm" ''
+    echo "npm not available use pnpm instead"
+    exit 1
+  '';
 in
 {
   options.modules.dev.node = {
     enable = lib.mkEnableOption "";
   };
 
-  config = mkIf cfg.enable {
-    environment.persistence."/persist" = mkIf withImpermanence {
+  config = lib.mkIf cfg.enable {
+    environment.persistence."/persist" = lib.mkIf withImpermanence {
       users.${username} = {
         directories = [
           ".cache/bun"
           ".cache/node"
-          ".cache/npm"
           ".cache/yarn"
-          ".config/npm"
           ".config/yarn"
           ".local/share/bun"
-          ".local/share/npm"
           ".local/share/pnpm"
           ".local/share/volta"
           ".local/share/yarn"
@@ -36,22 +39,18 @@ in
     };
 
     myhm = _: {
-      home.packages = with pkgs; [
-        nodejs_24
+      home.packages = [
+        (lib.hiPrio npmUnavailable)
+        nodejs
+        pnpm
         yarn
-        bun
-        deno
+        pkgs.bun
+        pkgs.deno
       ];
       home.sessionVariables = {
         # https://consoledonottrack.com/
         # https://bun.sh/docs/runtime/bunfig#telemetry
         DO_NOT_TRACK = "1";
-        NPM_CONFIG_USERCONFIG = "$XDG_CONFIG_HOME/npm/config";
-        NPM_CONFIG_CACHE = "$XDG_CACHE_HOME/npm";
-        NPM_CONFIG_PREFIX = "$XDG_CACHE_HOME/npm";
-        NPM_CONFIG_LOGS_DIR = "$XDG_STATE_HOME/npm/logs";
-        NPM_CONFIG_UPDATE_NOTIFIER = "false"; # Prevents ~/.npm/_update-notifier-last-checked
-        NPM_CONFIG_TMP = "$XDG_RUNTIME_DIR/npm";
         NODE_REPL_HISTORY = "$XDG_CACHE_HOME/node/repl_history";
         BUN_INSTALL = "$XDG_DATA_HOME/bun";
         BUN_INSTALL_CACHE_DIR = "$XDG_CACHE_HOME/bun";
@@ -60,15 +59,6 @@ in
         YARN_GLOBAL_FOLDER = "$XDG_DATA_HOME/yarn";
         VOLTA_HOME = "$XDG_DATA_HOME/volta";
       };
-      xdg.configFile."npm/config".text = ''
-        cache=''${XDG_CACHE_HOME}/npm
-        prefix=''${XDG_DATA_HOME}/npm
-        tmp=''${XDG_RUNTIME_DIR}/npm
-        init-module=''${XDG_CONFIG_DIR}/npm/config/npm-init.js
-        min-release-age=7
-        ignore-scripts=true
-      '';
-
       xdg.configFile."pnpm/rc".text = ''
         minimum-release-age=10080
       '';
