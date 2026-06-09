1b138cef74b3aa91d9c3c739be610222282e197a
Casey Link <unnamedrambler@gmail.com>
2026-06-09 09:58:42 +0200

    goodbye logseq
    - very glitch on wayland
    - often using EOLed electron
    
    switched to org files

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -14,16 +14,6 @@ _final: prev: {
   #  }
   #);
 
-  #logseq = prev.logseq.overrideAttrs (oldAttrs: {
-  #  postFixup = ''
-  #    makeWrapper ${prev.electron}/bin/electron $out/bin/${oldAttrs.pname} \
-  #      --set "LOCAL_GIT_DIRECTORY" ${prev.git} \
-  #      --add-flags $out/share/${oldAttrs.pname}/resources/app \
-  #      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
-  #      --prefix LD_LIBRARY_PATH : "${prev.lib.makeLibraryPath [ prev.stdenv.cc.cc.lib ]}"
-  #  '';
-  #});
-
   #quickemu = prev.quickemu.overrideAttrs (oldAttrs: {
   #  postPatch =
   #    (oldAttrs.postPatch or "")
