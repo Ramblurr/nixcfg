45c6c5f71f88f507ec89764830a873bd8e30b1a6
Casey Link <unnamedrambler@gmail.com>
2026-06-10 16:48:28 +0200

    mali: add numtide cache to ncps

--- hosts/mali/ncps.nix
+++ hosts/mali/ncps.nix
@@ -48,11 +48,13 @@ in
       caches = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
+        "https://cache.numtide.com"
         config.repo.secrets.global.localAtticSubstituter
       ];
       publicKeys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
         config.repo.secrets.global.localAtticPublicKey
       ];
     };
