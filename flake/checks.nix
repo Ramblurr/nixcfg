29e714391b95e6fe96d29bea6431f57f36eb93fd
Casey Link <unnamedrambler@gmail.com>
2026-07-22 13:43:45 +0200

    linux-voice-assistant: support announcement-only hosts

--- flake/checks.nix
+++ flake/checks.nix
@@ -3,8 +3,13 @@
   perSystem =
     { pkgs, ... }:
     {
-      checks.hindsight = import ../tests/hindsight.nix {
-        inherit inputs pkgs;
+      checks = {
+        hindsight = import ../tests/hindsight.nix {
+          inherit inputs pkgs;
+        };
+        linux-voice-assistant = import ../tests/linux-voice-assistant.nix {
+          inherit inputs pkgs;
+        };
       };
     };
 }
