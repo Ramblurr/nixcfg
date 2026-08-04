5c829883e12228bb15d0fb78eee77ab19f5a4ca7
Casey Link <unnamedrambler@gmail.com>
2026-08-04 18:43:14 +0200

    james: stabilize work webhook socket lifecycle

--- flake/checks.nix
+++ flake/checks.nix
@@ -12,6 +12,7 @@
           inherit inputs pkgs;
         };
         james-crowdsec = import ../tests/james-crowdsec.nix { inherit inputs pkgs; };
+        james-webhook = import ../tests/james-webhook.nix { inherit inputs pkgs; };
         local-issues = import ../tests/local-issues.nix { inherit inputs pkgs; };
         linux-voice-assistant = import ../tests/linux-voice-assistant.nix {
           inherit inputs pkgs;
