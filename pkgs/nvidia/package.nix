0466e3c69977c5e33da80c51915702069f72d745
Casey Link <unnamedrambler@gmail.com>
2026-08-04 11:16:48 +0200

    update(pkgs): Update sources of all downstream packages

--- pkgs/nvidia/package.nix
+++ pkgs/nvidia/package.nix
@@ -5,9 +5,9 @@
   kernelPackages,
 }:
 (kernelPackages.nvidiaPackages.mkDriver {
-  version = "595.71.05";
-  sha256_64bit = "sha256-NiA7iWC35JyKQva6H1hjzeNKBek9KyS3mK8G3YRva4I=";
-  openSha256 = "sha256-Lfz71QWKM6x/jD2B22SWpUi7/og30HRlXg1kL3EWzEw=";
+  version = "595.91.07";
+  sha256_64bit = "sha256-yiPIjdJLB6GRZE4eEc+3vN11NzBXSa9A+YABiwleYxM=";
+  openSha256 = "sha256-OB8Epd+qn/WywxsPiFpxEOAzlJqb6I1SyRoV3a8l71k=";
 
   useSettings = false;
   usePersistenced = false;
