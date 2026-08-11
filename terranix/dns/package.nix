bbf6ad175030b78f29375d1b1f9d667b095090af
Casey Link <unnamedrambler@gmail.com>
2026-08-11 10:59:00 +0200

    terranix/dns: compile private logical records

--- terranix/dns/package.nix
+++ terranix/dns/package.nix
@@ -1,11 +1,12 @@
 {
   pkgs,
   zones,
+  records ? [ ],
 }:
 let
   terranix = import "${pkgs.terranix}/core/default.nix" {
     inherit pkgs;
-    modules = [ (import ./default.nix { inherit zones; }) ];
+    modules = [ (import ./default.nix { inherit records zones; }) ];
   };
 in
 {
