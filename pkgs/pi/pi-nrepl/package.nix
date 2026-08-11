bd3043189173af40b874e396f5ae8ac92e239e69
Casey Link <unnamedrambler@gmail.com>
2026-08-11 15:50:59 +0200

    pi-nrepl: package extension

---
+++ pkgs/pi/pi-nrepl/package.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  fetchFromGitHub,
+  stdenvNoCC,
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "pi-nrepl";
+  version = "0.1.0";
+
+  src = fetchFromGitHub {
+    owner = "ramblurr";
+    repo = "pi-nrepl";
+    rev = "d173f0427bb64d54160f12d5e0aad67403bf1f05";
+    hash = "sha256-RoLC0blH0JRF1bWUQa0zHc/gnUoNBlvSkFx6f9ufIBY=";
+  };
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p "$out"
+    cp -r package.json dist "$out"
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Pi nREPL debugging extension";
+    homepage = "https://github.com/ramblurr/pi-nrepl";
+    license = lib.licenses.eupl12;
+    platforms = lib.platforms.all;
+  };
+})
