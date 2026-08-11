2e8c3c390ca51797dab010665fa17d31b1ded4e6
Casey Link <unnamedrambler@gmail.com>
2026-08-11 17:48:09 +0200

    pi-nono-sandbox: package extension

---
+++ pkgs/pi/pi-nono-sandbox/package.nix
@@ -0,0 +1,31 @@
+{
+  fetchFromGitHub,
+  lib,
+  stdenvNoCC,
+}:
+
+stdenvNoCC.mkDerivation {
+  pname = "pi-nono-sandbox";
+  version = "0.1.0-unstable-2026-08-11";
+
+  src = fetchFromGitHub {
+    owner = "Ramblurr";
+    repo = "pi-extensions";
+    rev = "27393860d3b8b7cf031e5e02a0684f7f477e5c04";
+    hash = "sha256-oPWEdkZfnxY0uo5dkx55/GWNCTEM8dtzXIFz4SLDZ34=";
+  };
+  sourceRoot = "source/pi-nono-sandbox";
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p "$out"
+    cp -r . "$out"
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Nono sandbox diagnostic tools for Pi";
+    homepage = "https://github.com/Ramblurr/pi-extensions";
+    platforms = lib.platforms.all;
+  };
+}
