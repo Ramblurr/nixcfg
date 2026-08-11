848fd800f77dd00f50c55ce008fd2a1ff75ea879
Casey Link <unnamedrambler@gmail.com>
2026-08-11 17:48:09 +0200

    pi-link-control: package extension

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -31,6 +31,7 @@ inputs: [
       pi-reload = prev.callPackage ./pi/pi-reload/package.nix { };
       pi-ghost = prev.callPackage ./pi/pi-ghost/package.nix { };
       brepl-balance = prev.callPackage ./pi/brepl-balance/package.nix { };
+      pi-link-control = prev.callPackage ./pi/pi-link-control/package.nix { };
       pi-nono-sandbox = prev.callPackage ./pi/pi-nono-sandbox/package.nix { };
       pi-openai-fast = prev.callPackage ./pi/pi-openai-fast/package.nix { };
       pi-sexp-edit = prev.callPackage ./pi/pi-sexp-edit/package.nix { };
