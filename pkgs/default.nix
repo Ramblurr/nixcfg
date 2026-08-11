7c72e896cab1f1a87ce5ed361982438a76729383
Casey Link <unnamedrambler@gmail.com>
2026-08-11 16:03:24 +0200

    plannotator: package Pi extension

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -24,6 +24,7 @@ inputs: [
       pi-web = prev.callPackage ./pi-web/package.nix { };
       pi-nrepl = prev.callPackage ./pi/pi-nrepl/package.nix { };
       pi-hashline-edit = prev.callPackage ./pi/pi-hashline-edit/package.nix { };
+      plannotator-pi-extension = prev.callPackage ./pi/plannotator/package.nix { };
       glimpseui = prev.callPackage ./glimpseui/package.nix { };
       udpbroadcastrelay = prev.callPackage ./udpbroadcastrelay.nix { };
       brepl = prev.lib.callPackageWith (prev // { inherit pkgs-lib; }) ./brepl/package.nix { };
---
+++ pkgs/pi/plannotator/package.nix
@@ -0,0 +1,40 @@
+{
+  buildNpmPackage,
+  fetchurl,
+  lib,
+}:
+
+buildNpmPackage (finalAttrs: {
+  pname = "plannotator-pi-extension";
+  version = "0.24.2";
+
+  src = fetchurl {
+    url = "https://registry.npmjs.org/@plannotator/pi-extension/-/pi-extension-0.24.2.tgz";
+    hash = "sha256-SNh4ACqFUdeTF2dx35v63M3dpFWO+awxsGSIIEIxPRc=";
+  };
+  sourceRoot = "package";
+
+  postPatch = ''
+    cp ${./package-lock.json} package-lock.json
+  '';
+
+  npmDepsHash = "sha256-aV5eJ4l5h3AozFrUCFL0FBwwl3FSjIucKYPqXFC5e2I=";
+  npmFlags = [ "--legacy-peer-deps" ];
+  npmInstallFlags = [ "--omit=dev" ];
+  npmRebuildFlags = [ "--ignore-scripts" ];
+  dontNpmBuild = true;
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p "$out"
+    cp -r . "$out"
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Interactive plan review extension for Pi";
+    homepage = "https://github.com/backnotprop/plannotator";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.all;
+  };
+})
