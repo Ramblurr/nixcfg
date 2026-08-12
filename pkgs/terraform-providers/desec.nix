9c7ccc37d77e3b2d226c80e57e116279f57bd73e
Casey Link <unnamedrambler@gmail.com>
2026-08-12 15:11:00 +0200

    terraform-providers: add local update scripts

--- pkgs/terraform-providers/desec.nix
+++ pkgs/terraform-providers/desec.nix
@@ -1,7 +1,11 @@
 {
+  git,
+  nix,
+  nix-update,
+  pkgs-lib,
   terraform-providers,
 }:
-terraform-providers.mkProvider {
+(terraform-providers.mkProvider {
   owner = "timofurrer";
   repo = "terraform-provider-desec";
   rev = "v0.6.3";
@@ -10,4 +14,17 @@ terraform-providers.mkProvider {
   homepage = "https://registry.terraform.io/providers/timofurrer/desec";
   provider-source-address = "registry.terraform.io/timofurrer/desec";
   spdx = "MPL-2.0";
-}
+}).overrideAttrs
+  (old: {
+    passthru = old.passthru // {
+      updateScript = pkgs-lib.writeUpdateScript {
+        packageToUpdate = "terraform-provider-desec";
+        utils = [
+          git
+          nix
+          nix-update
+        ];
+        script = ./update.bb;
+      };
+    };
+  })
