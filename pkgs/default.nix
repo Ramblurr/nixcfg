c7834da814690b27af61e5a6c77a3b5cb6a359a9
Casey Link <unnamedrambler@gmail.com>
2026-08-14 14:13:17 +0200

    caddy: add security plugin package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,6 +6,11 @@ inputs: [
       pkgs-lib = prev.callPackage ../lib/pkgs.nix { flake-inputs = inputs; };
     in
     {
+      # caddy-security v1.1.64, commit b96087fc696ada2ed80f4ca81e031878a47d43ac
+      caddy-with-security = prev.caddy.withPlugins {
+        plugins = [ "github.com/greenpau/caddy-security@v1.1.64" ];
+        hash = "sha256-QoLRkDSZky2nqwq04EdWVe6pnUBuVnVj9xNaG7z3QxY=";
+      };
       nvidia = prev.lib.callPackageWith (prev // { inherit pkgs-lib; }) ./nvidia/package.nix {
         kernelPackages = prev.linuxPackages;
       };
