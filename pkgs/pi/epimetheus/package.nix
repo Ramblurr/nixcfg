04be13c4fb309bfcc41bd5c3aed429d32d0d47e3
Casey Link <casey@outskirtslabs.com>
2026-08-15 20:17:05 +0200

    epimetheus: 0.6.0 -> 0.6.1

--- pkgs/pi/epimetheus/package.nix
+++ pkgs/pi/epimetheus/package.nix
@@ -6,20 +6,19 @@
 
 buildNpmPackage (_finalAttrs: {
   pname = "epimetheus";
-  version = "0.6.0";
+  version = "0.6.1";
 
   src = fetchFromGitHub {
     owner = "noctuid";
     repo = "epimetheus";
-    rev = "b3dacfa21cc7a5e8b6a9080f68fa36540a44bdcf";
-    hash = "sha256-vUOTuwaIe6Yyq9Vu7s2lxyDtPeV3UHsng/Yg8uxS+E4=";
+    rev = "51a7730561a7588d74929f9130bff3134dc43bb2";
+    hash = "sha256-XSdo0SVWxR1hNnimQ3N4yZr11Hl8JbKFNNpfAV98/PA=";
   };
 
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
   '';
-
-  npmDepsHash = "sha256-QXp+lW5fsjhrAQPSdcOhSZ394SCHpaYiMkQGa1UiDdg=";
+  npmDepsHash = "sha256-GCpqNsIJcH3PV37EisfhcQ+8PvHBcLczi9+vbWyf5BM=";
   npmFlags = [ "--legacy-peer-deps" ];
   npmInstallFlags = [ "--omit=dev" ];
   npmRebuildFlags = [ "--ignore-scripts" ];
