c7b388f84ccb4b1baf4b3dd1455a0f360e1e12e7
Casey Link <unnamedrambler@gmail.com>
2026-08-18 09:30:52 +0200

    podman: centralize host enablement and maintenance

--- hosts/addams/modules/podman.nix
+++ hosts/addams/modules/podman.nix
@@ -1,26 +1,20 @@
 _: {
-  virtualisation = {
-    containers.enable = true;
-    oci-containers.backend = "podman";
-    podman = {
-      enable = true;
-      defaultNetwork.settings = {
-        network_interface = "ctr0";
-        # setting this to true ironically breaks the dns because dns server is not running in the gateway ip
-        # even setting `dns_name_servers` here doesn't work with this set to true
-        dns_enabled = false;
-        subnets = [
-          # change these
-          {
-            subnet = "10.12.0.0/24";
-            gateway = "10.12.0.1";
-            lease_range = {
-              start_ip = "10.12.0.50";
-              end_ip = "10.12.0.200";
-            };
-          }
-        ];
-      };
-    };
+  modules.services.podman.enable = true;
+  virtualisation.podman.defaultNetwork.settings = {
+    network_interface = "ctr0";
+    # setting this to true ironically breaks the dns because dns server is not running in the gateway ip
+    # even setting `dns_name_servers` here doesn't work with this set to true
+    dns_enabled = false;
+    subnets = [
+      # change these
+      {
+        subnet = "10.12.0.0/24";
+        gateway = "10.12.0.1";
+        lease_range = {
+          start_ip = "10.12.0.50";
+          end_ip = "10.12.0.200";
+        };
+      }
+    ];
   };
 }
