6346da1c51e89a492dc9c5917daa1db50cddbab4
Casey Link <unnamedrambler@gmail.com>
2026-08-21 09:42:05 +0200

    zrepl reconciler: test DynamicUser runtime integration #035-04

--- flake/checks.nix
+++ flake/checks.nix
@@ -31,6 +31,7 @@
         mali-garage = import ../tests/mali-garage.nix { inherit inputs pkgs; };
         mali-zrepl-retention = import ../tests/mali-zrepl-retention.nix { inherit inputs pkgs; };
         mali-zrepl-reconcile = import ../tests/mali-zrepl-reconcile.nix { inherit inputs pkgs; };
+        mali-zrepl-reconcile-vm = import ../tests/mali-zrepl-reconcile-vm.nix { inherit pkgs; };
         zrepl-outage-continuity = import ../tests/zrepl-outage-continuity.nix { inherit pkgs; };
         common-server = import ../tests/common-server.nix { inherit inputs pkgs; };
         debord-monitoring = import ../tests/debord-monitoring.nix { inherit inputs pkgs; };
