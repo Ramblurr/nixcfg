46124e008b27a8972183d93bbcb5e31dc0f2a411
Casey Link <unnamedrambler@gmail.com>
2026-08-21 10:43:16 +0200

    nixbot: add logical database backups #046-06

--- hosts/debord/nixbot.nix
+++ hosts/debord/nixbot.nix
@@ -14,6 +14,10 @@ let
   inherit (self.inputs.nixbot.lib) interpolate;
 
   nixbotPort = config.repo.secrets.home-ops.ports.nixbot;
+  backupRole = "databasus_nixbot";
+  databaseName = "nixbot";
+  maliMgmtAddress = builtins.head config.site.net.mgmt.hosts4.mali;
+  debordMgmtAddress = builtins.head config.site.net.mgmt.hosts4.debord;
   workDomain = config.repo.secrets.global.domain.work;
   localAtticSubstituter = config.repo.secrets.global.localAtticSubstituter;
   quineMgmtIp = builtins.head config.site.net.mgmt.hosts4.quine;
@@ -51,6 +55,30 @@ let
   '';
 in
 {
+  assertions = [
+    {
+      assertion = config.modules.services.onepassword-systemd-credentials.enable;
+      message = "Nixbot database backup credentials require the systemd credential provider.";
+    }
+    {
+      assertion = !(builtins.elem 5432 config.networking.firewall.allowedTCPPorts);
+      message = "Nixbot PostgreSQL must not be exposed through globally allowed TCP port 5432.";
+    }
+  ];
+
+  modules.services.onepassword-systemd-credentials.consumers.databasus-nixbot-role = {
+    POSTGRES_PASSWORD = "op://home-ops-prod/databasus-nixbot/password";
+  };
+
+  services.postgresql = {
+    enableTCPIP = true;
+    settings.listen_addresses = lib.mkForce debordMgmtAddress;
+    authentication = lib.mkAfter ''
+      host ${databaseName} ${backupRole} ${maliMgmtAddress}/32 scram-sha-256
+      host all ${backupRole} ${maliMgmtAddress}/32 reject
+    '';
+  };
+
   services.nixbot = {
     enable = true;
     domain = "ci.${workDomain}";
@@ -105,6 +133,57 @@ in
     "attic-nixbot-token:${config.sops.secrets."attic-nixbot-token".path}"
   ];
 
+  systemd.services.databasus-nixbot-role = {
+    description = "Provision the Databasus read-only role for Nixbot";
+    wantedBy = [ "multi-user.target" ];
+    requires = [ "postgresql.service" ];
+    after = [ "postgresql.service" ];
+    script = ''
+      ${config.services.postgresql.package}/bin/psql \
+        --no-psqlrc \
+        --quiet \
+        --set ON_ERROR_STOP=1 \
+        --dbname postgres <<'SQL'
+      \set password `cat "$CREDENTIALS_DIRECTORY/POSTGRES_PASSWORD"`
+      SELECT 'CREATE ROLE ${backupRole} LOGIN'
+      WHERE NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '${backupRole}') \gexec
+      ALTER ROLE ${backupRole}
+        WITH LOGIN NOSUPERUSER NOCREATEDB NOCREATEROLE NOREPLICATION NOBYPASSRLS;
+      SELECT format('ALTER ROLE %I PASSWORD %L', '${backupRole}', :'password') \gexec
+      REVOKE ALL PRIVILEGES ON DATABASE ${databaseName} FROM ${backupRole};
+      GRANT CONNECT ON DATABASE ${databaseName} TO ${backupRole};
+      SQL
+
+      ${config.services.postgresql.package}/bin/psql \
+        --no-psqlrc \
+        --quiet \
+        --set ON_ERROR_STOP=1 \
+        --dbname ${databaseName} <<'SQL'
+      REVOKE ALL PRIVILEGES ON SCHEMA public FROM ${backupRole};
+      GRANT USAGE ON SCHEMA public TO ${backupRole};
+      REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM ${backupRole};
+      GRANT SELECT ON ALL TABLES IN SCHEMA public TO ${backupRole};
+      REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM ${backupRole};
+      GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO ${backupRole};
+      ALTER DEFAULT PRIVILEGES FOR ROLE nixbot IN SCHEMA public
+        REVOKE ALL PRIVILEGES ON TABLES FROM ${backupRole};
+      ALTER DEFAULT PRIVILEGES FOR ROLE nixbot IN SCHEMA public
+        GRANT SELECT ON TABLES TO ${backupRole};
+      ALTER DEFAULT PRIVILEGES FOR ROLE nixbot IN SCHEMA public
+        REVOKE ALL PRIVILEGES ON SEQUENCES FROM ${backupRole};
+      ALTER DEFAULT PRIVILEGES FOR ROLE nixbot IN SCHEMA public
+        GRANT SELECT ON SEQUENCES TO ${backupRole};
+      SQL
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "postgres";
+      Group = "postgres";
+      RemainAfterExit = true;
+      ExecStartPre = "${pkgs.coreutils}/bin/test -S /run/postgresql/.s.PGSQL.5432";
+    };
+  };
+
   # Root is impermanent; keep service state and the CI database on safe
   # datasets. The postgresql dataset is already declared in disk-config.nix;
   # declaring it here as well lets zfs-datasets create it if it is missing.
@@ -116,6 +195,9 @@ in
   };
 
   networking.firewall.allowedTCPPorts = [ nixbotPort ];
+  networking.firewall.extraInputRules = ''
+    iifname "mgmt" ip saddr ${maliMgmtAddress}/32 ip daddr ${debordMgmtAddress} tcp dport 5432 accept comment "Databasus Nixbot logical backup"
+  '';
   networking.hosts.${quineMgmtIp} = [ "quine" ];
 
   # Remote builders: quine acts as an x86_64-linux build machine.
