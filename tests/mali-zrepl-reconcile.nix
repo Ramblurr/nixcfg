b0c7a0d1820df33d98b19f1db99cc1abd3d5f87d
Casey Link <unnamedrambler@gmail.com>
2026-08-27 17:07:43 +0200

    rsyncnet/zrepl: make package plans noninteractive #035-05

--- tests/mali-zrepl-reconcile.nix
+++ tests/mali-zrepl-reconcile.nix
@@ -82,7 +82,7 @@ assert
     compression = "zstd";
     mountpoint = "/var/lib/private/rsyncnet-zrepl-reconcile";
   };
-assert service.environment.EXPECTED_BUNDLE_ID == "v1-7edc60fffa7eed56-initial";
+assert service.environment.EXPECTED_BUNDLE_ID == "v1-fd0949b92d3537f0-initial";
 assert service.environment.SSH_DEADLINE_SECONDS == "900";
 assert !(service.environment ? STATE_DIRECTORY);
 assert !(service.environment ? RUNTIME_DIRECTORY);
