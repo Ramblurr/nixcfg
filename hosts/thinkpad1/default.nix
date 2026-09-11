bb665d4fba83f0869112e8dbac821667ae9d2409
Casey Link <unnamedrambler@gmail.com>
2026-09-11 14:13:31 +0200

    thinkpad1: enable onedrive

--- hosts/thinkpad1/default.nix
+++ hosts/thinkpad1/default.nix
@@ -186,4 +186,18 @@ in
       ColorScheme=Catppuccin${catppuccinName}${catppuccinAccentName}
     '';
   };
+  services.onedrive.enable = true;
+  # create config file in ~/.config
+  myhm = {
+    xdg.configFile."onedrive/config".text = ''
+      # # try to download changes from onedrive every x seconds
+      # monitor_interval = "6"
+      # # fully scan data for integrity every x attempt of downloading (monitor_interval)
+      # monitor_fullscan_frequency = "50"
+      # # minimum number of downloaded changes to trigger desktop notification
+      # min_notify_changes = "1"
+      # ignore temporary stuff
+      skip_file = "~*|.~*|*.tmp"
+    '';
+  };
 }
