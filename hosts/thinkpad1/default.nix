1f955b88c903e00f2f989caef861b13f1b546930
Casey Link <unnamedrambler@gmail.com>
2026-09-11 14:33:49 +0200

    thinkpad1: set Viki's OneDrive sync directory

--- hosts/thinkpad1/default.nix
+++ hosts/thinkpad1/default.nix
@@ -190,13 +190,7 @@ in
   # create config file in ~/.config
   myhm = {
     xdg.configFile."onedrive/config".text = ''
-      # # try to download changes from onedrive every x seconds
-      # monitor_interval = "6"
-      # # fully scan data for integrity every x attempt of downloading (monitor_interval)
-      # monitor_fullscan_frequency = "50"
-      # # minimum number of downloaded changes to trigger desktop notification
-      # min_notify_changes = "1"
-      # ignore temporary stuff
+      sync_dir = "/home/viki/OneDrive"
       skip_file = "~*|.~*|*.tmp"
     '';
   };
